403666b7e68f53680ffd037b9fe36c1124859a76
markmeliaa <marcaalconmelia@gmail.com>
2022-12-23 22:10:56 +0100

    feat: Update GameObject Components in the GameObject Update

--- Source/DataModels/GameObject/GameObject.cpp
+++ Source/DataModels/GameObject/GameObject.cpp
@@ -18,7 +18,10 @@ GameObject::~GameObject()
 
 void GameObject::Update()
 {
-
+	for (int i = 0; i < components.size(); i++)
+	{
+		components[i]->Update();
+	}
 }
 
 Component* GameObject::CreateComponent(ComponentType type)
