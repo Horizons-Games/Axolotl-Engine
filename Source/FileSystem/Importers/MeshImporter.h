9a87de5a0a374ae827a2b8b504cde84c3d5c39b1
Eric <eric@dacal.es>
2022-12-24 00:02:46 +0100

    feat: Implementation of header for MeshImporter

---
+++ Source/FileSystem/Importers/MeshImporter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "FileSystem/Importers/Importer.h"
+#include "FileSystem/Data.h"
+#include "assimp/mesh.h"
+
+
+class MeshImporter : public Importer<aiMesh, DataMesh>
+{
+public:
+    MeshImporter() = default;
+    ~MeshImporter() { delete[] buffer; }
+
+    void Import(const aiMesh* mesh, DataMesh* ourMesh) override;
+    uint64_t Save(const DataMesh* ourMesh, char** fileBuffer) override;
+    void Load(const char* fileBuffer, DataMesh* ourMesh) override;
+
+private:
+    char* buffer;
+};
