c36b2507fdb77a7d8712cd016e38d715c4e9da6a
Gerard <gqueraltferre@gmail.com>
2022-12-24 21:44:01 +0100

    feat: created ResourceTexture class
    the cpp file is empty because I want to discuss how we want to set the members (width, format, etc.). Do we add them to the constructor, meaning they'll be computed in the Module, or do we compute them inside the ResourceTexture class? In the first case the cpp won't be needed, but in the second one it will

---
+++ Source/DataModels/Resources/ResourceTexture.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "Resource.h"
+
+enum class TextureFormat
+{
+	Unknown,
+	RGB,
+	RGBA,
+	BGR,
+	BGRA,
+	Luminance
+};
+
+class ResourceTexture : public Resource
+{
+public:
+	ResourceTexture(int resourceUUID, const std::string& assetsPath, const std::string& libraryPath);
+	~ResourceTexture() override = default;
+
+	ResourceType GetType() const override;
+	unsigned int GetWidth() const;
+	unsigned int GetHeight() const;
+	TextureFormat GetFormat() const;
+
+private:
+	unsigned int width = 0;
+	unsigned int height = 0;
+	TextureFormat format = TextureFormat::Unknown;
+};
+
+inline ResourceTexture::ResourceTexture(int resourceUUID,
+										const std::string& assetsPath,
+										const std::string& libraryPath) :
+	Resource(resourceUUID, assetsPath, libraryPath)
+{
+}
+
+inline ResourceType ResourceTexture::GetType() const
+{
+	return ResourceType::Texture;
+}
+
+inline unsigned int ResourceTexture::GetWidth() const
+{
+	return width;
+}
+
+inline unsigned int ResourceTexture::GetHeight() const
+{
+	return height;
+}
+
+inline TextureFormat ResourceTexture::GetFormat() const
+{
+	return format;
+}
