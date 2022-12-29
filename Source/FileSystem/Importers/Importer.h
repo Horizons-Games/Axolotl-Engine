18bfc406c84b383d541eabc34dc1fec957b0f4af
Eric <eric@dacal.es>
2022-12-29 10:19:01 +0100

    refactor: Change parameters in Importer base class

--- Source/FileSystem/Importers/Importer.h
+++ Source/FileSystem/Importers/Importer.h
@@ -14,7 +14,7 @@ public:
 	// Reads assimp data and generates your own data
 	virtual void Import(const T* material, U* ourMaterial) = 0;
 	// Reads your own data and generates a file buffer
-	virtual uint64_t Save(const U* ourMaterial, char** fileBuffer) = 0;
+	virtual uint64_t Save(const U* ourMaterial, char* &fileBuffer) = 0;
 	// Reads a file buffer and generates your own data
 	virtual void Load(const char* fileBuffer, U* ourMaterial) = 0;
 };
