e1fccaf4930ab7f75bb134c38f91bdb867796c3f
Piterga12 <pitergatf@gmail.com>
2022-12-29 18:09:25 +0100

    feat: Add Unique ID
    Added Functional Unique ID (UniqueID.cpp and UniqueID.h)

---
+++ Source/UniqueID.cpp
@@ -0,0 +1,51 @@
+#include "UniqueID.h"
+#include <sstream>
+
+//For using only include UniqueID.h and add UID uid = UniqueID::GenerateUID(); to create a number UID 
+    //and std::string uid = UniqueID::GenerateUUID(); to create a number+letter UID
+
+UniqueID::~UniqueID() 
+{
+}
+
+UID UniqueID::GenerateUID() 
+{
+	return uidNew(creator);
+}
+
+std::string UniqueID::GenerateUUID() 
+{
+	std::stringstream ss; //read/write the string
+	ss << std::hex;
+	ss << std::uppercase;
+
+	int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        ss << uuidNew(creator);
+    }
+    ss << "-";
+    for (i = 0; i < 4; i++)
+    {
+        ss << uuidNew(creator);
+    }
+    ss << "-";
+
+    for (i = 0; i < 3; i++)
+    {
+        ss << uuidNew(creator);
+    }
+    ss << "-";
+
+    for (i = 0; i < 3; i++)
+    {
+        ss << uuidNew(creator);
+    }
+    ss << "-";
+    for (i = 0; i < 12; i++)
+    {
+        ss << uuidNew(creator);
+    }
+    return ss.str();
+}
