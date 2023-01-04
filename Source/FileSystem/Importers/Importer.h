b89362e8abb75b037f446e99da887271ac6250d8
Eric <eric@dacal.es>
2023-01-04 19:08:34 +0100

    fix: Fix problem that comes from previous conflict resolution

--- Source/FileSystem/Importers/Importer.h
+++ Source/FileSystem/Importers/Importer.h
@@ -13,13 +13,13 @@ public:
 	}
 	// Reads a path that indicates a file to import to engine
 	virtual void Start(const char* filePath, U* resource);
+	// Reads your own data and generates a file buffer
+	virtual uint64_t Save(const U* resource, char*& fileBuffer) = 0;
+	// Reads assimp data and generates your own data
+	virtual void Load(const char* fileBuffer, U* resource) = 0;
 
 private:
 	// Reads assimp data and generates your own data
 	virtual void Import(const T* material, U* resource) = 0;
-	// Reads your own data and generates a file buffer
-	virtual uint64_t Save(const U* resource, char*& fileBuffer) = 0;
-private:
-	// Reads assimp data and generates your own data
-	virtual void Load(const char* fileBuffer, U* resource) = 0;
+	
 };
