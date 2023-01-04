da8c12099515a712d0c3da84383d8c1cbcb8e4ec
Eric <eric@dacal.es>
2023-01-04 08:03:50 +0100

    refactor: change visibility of Import, Load and Save to private in Importer base class

--- Source/FileSystem/Importers/Importer.h
+++ Source/FileSystem/Importers/Importer.h
@@ -13,10 +13,11 @@ public:
 	}
 	// Reads a path that indicates a file to import to engine
 	virtual void Start(const char* filePath);
+private:
 	// Reads assimp data and generates your own data
 	virtual void Import(const T* material, U* ourMaterial) = 0;
 	// Reads your own data and generates a file buffer
-	virtual uint64_t Save(const U* ourMaterial, char* &fileBuffer) = 0;
+	virtual uint64_t Save(const U* ourMaterial, char*& fileBuffer) = 0;
 	// Reads a file buffer and generates your own data
 	virtual void Load(const char* fileBuffer, U* ourMaterial) = 0;
 };
