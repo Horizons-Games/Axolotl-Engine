cfef153393cf77178b461f977199bb18ebd7fe80
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-01-07 20:02:07 +0100

    feat[TextureImporter]: Format Texture loaded on Importer

--- Source/FileSystem/Importers/TextureImporter.cpp
+++ Source/FileSystem/Importers/TextureImporter.cpp
@@ -3,6 +3,7 @@
 #include "EngineLog.h"
 #include "FileSystem/Data.h"
 
+#include <GL/glew.h>
 #include <DirectXTex/DirectXTex.h>
 
 void TextureImporter::Start(const char* filePath, DataTexture* ourTexture)
@@ -46,13 +47,46 @@ void TextureImporter::Start(const char* filePath, DataTexture* ourTexture)
 
 	result = DirectX::SaveToDDSFile(img.GetImages(), img.GetImageCount(), img.GetMetadata(), DirectX::DDS_FLAGS_NONE, path);
 
-	//TODO Call Resource Start or Init function with all this info
+	GLint internalFormat;
+	GLenum format, type;
+
+	switch (img.GetMetadata().format)
+	{
+	case DXGI_FORMAT_R8G8B8A8_UNORM_SRGB:
+	case DXGI_FORMAT_R8G8B8A8_UNORM:
+		internalFormat = GL_RGBA8;
+		format = GL_RGBA;
+		type = GL_UNSIGNED_BYTE;
+		break;
+	case DXGI_FORMAT_B8G8R8A8_UNORM_SRGB:
+	case DXGI_FORMAT_B8G8R8A8_UNORM:
+		internalFormat = GL_RGBA8;
+		format = GL_BGRA;
+		type = GL_UNSIGNED_BYTE;
+		break;
+	case DXGI_FORMAT_B5G6R5_UNORM:
+		internalFormat = GL_RGB8;
+		format = GL_BGR;
+		type = GL_UNSIGNED_BYTE;
+		break;
+	case DXGI_FORMAT_BC1_UNORM:
+		internalFormat = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+		format = GL_RGBA;
+		type = GL_UNSIGNED_BYTE;
+		break;
+	default:
+		assert(false && "Unsupported format");
+	}
+
 	ourTexture->width = img.GetMetadata().width;
 	ourTexture->height = img.GetMetadata().height;
 
-	ourTexture->format = img.GetMetadata().format;
-	ourTexture->dataSize = img.GetPixelsSize();
-	ourTexture->imageData = img.GetPixels();
+	ourTexture->internalFormat = internalFormat;
+	ourTexture->format = format;
+	ourTexture->type = type;
+
+	ourTexture->pixelsSize = img.GetPixelsSize();
+	ourTexture->pixels = img.GetPixels();
 
 	//Actualize metafile if needed
 }
@@ -64,10 +98,10 @@ uint64_t TextureImporter::Save(const DataTexture* ourTexture, char*& fileBuffer)
 		ourTexture->width,
 		ourTexture->height,
 		ourTexture->format,
-		ourTexture->dataSize
+		ourTexture->pixelsSize
 	};
 
-	unsigned int size = sizeof(header) + sizeof(unsigned char) * ourTexture->dataSize;
+	unsigned int size = sizeof(header) + sizeof(unsigned char) * ourTexture->pixelsSize;
 
 	char* cursor = new char[size];
 
@@ -78,8 +112,8 @@ uint64_t TextureImporter::Save(const DataTexture* ourTexture, char*& fileBuffer)
 
 	cursor += bytes;
 
-	bytes = sizeof(unsigned char) * ourTexture->dataSize;
-	memcpy(cursor, ourTexture->imageData, bytes);
+	bytes = sizeof(unsigned char) * ourTexture->pixelsSize;
+	memcpy(cursor, ourTexture->pixels, bytes);
 
 	// Provisional return, here we have to return serialize UID for the object
 	return 0;
@@ -93,11 +127,11 @@ void TextureImporter::Load(const char* fileBuffer, DataTexture* ourTexture)
 	ourTexture->width = header[0];
 	ourTexture->height = header[1];
 	ourTexture->format = header[2];
-	ourTexture->dataSize = header[3];
+	ourTexture->pixelsSize = header[3];
 
 	fileBuffer += sizeof(header);
 
-	ourTexture->imageData = new unsigned char[ourTexture->dataSize];
+	ourTexture->pixels = new unsigned char[ourTexture->pixelsSize];
 
-	memcpy(ourTexture->imageData, fileBuffer, sizeof(unsigned char) * ourTexture->dataSize);
+	memcpy(ourTexture->pixels, fileBuffer, sizeof(unsigned char) * ourTexture->pixelsSize);
 }
