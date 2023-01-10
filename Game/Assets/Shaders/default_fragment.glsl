e2770831adac25f5ddba8d899d443e6ef3a512f0
Oriol Murcia Catalan <oriolmurciacatalan@gmail.com>
2023-01-11 00:06:13 +0100

    feat[Shaders]: normals and normalMap

--- Game/Assets/Shaders/default_fragment.glsl
+++ Game/Assets/Shaders/default_fragment.glsl
@@ -1,6 +1,7 @@
 #version 440
 
-uniform sampler2D texDiffuse;
+out vec4 outColor;
+
 
 layout(std140) uniform Ambient
 {
@@ -13,11 +14,56 @@ layout(std140) uniform Directional
 	vec4 directionalColor;	//16	//16     // note: alpha parameter of colour is the intensity 
 };
 
-out vec4 color;
+uniform vec3 viewPos;
+uniform vec3 lightPos;
 
 in vec2 uv0;
+in vec3 fragPos;
+in vec3 fragNormal;
+in vec3 fragTangent;
+
+layout(binding = 0) uniform sampler2D diffuse;
+layout(binding = 1) uniform sampler2D normalMap;
+
+mat3 CreateTangentSpace(const vec3 normal, const vec3 tangent)
+{
+    vec3 orthoTangent = normalize(tangent - dot(tangent, normal) * normal);
+    vec3 bitangent = cross(orthoTangent, normal);
+    return transpose(mat3(orthoTangent, bitangent, normal)); //TBN
+}
 
 void main()
-{		
-	color = texture2D(texDiffuse, uv0);
+{
+	vec3 normal = fragNormal;
+    vec3 tangent = fragTangent;
+	
+	bool hasNormals = false;
+	if (hasNormals)
+	{
+        mat3 space = CreateTangentSpace(normalize(normal), normalize(tangent));
+        float nStrength = 1.0;
+        normal = texture(normalMap, uv0).rgb;
+        normal = normal * 2.0 - 1.0;
+        normal.xy *= nStrength;
+        normal = normalize(normal);
+        normal = space * normal;
+	}
+    // get diffuse color
+	vec3 color = texture2D(diffuse, uv0).rgb;
+	// ambient
+	vec3 ambient = 0.1 * color;
+
+    // diffuse 
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = 0.8 * diff * color;
+        
+    // specular
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+    vec3 specular = vec3(0.2) * spec;  
+        
+    vec3 result = ambient + diffuse + specular;
+    outColor = vec4(result, 1.0);
 }
--- Game/Assets/Shaders/default_vertex.glsl
+++ Game/Assets/Shaders/default_vertex.glsl
@@ -2,15 +2,28 @@
 
 layout(location = 0) in vec3 vertexPosition;
 layout(location = 1) in vec2 vertex_uv0;
+layout(location = 2) in vec3 aNormal;
+layout(location = 3) in vec3 aTangent;
 
 layout(location = 0) uniform mat4 proj;
 layout(location = 1) uniform mat4 view;
 layout(location = 2) uniform mat4 model;
 
 out vec2 uv0;
+out vec3 fragPos;
+out vec3 fragNormal;
+out vec3 fragTangent;
 
 void main()
 {
+    //normals
+    mat3 normalMatrix = transpose(inverse(mat3(model)));
+    fragNormal = normalize(normalMatrix * aNormal);
+    fragTangent = normalize(normalMatrix * aTangent);
+
+
+    fragPos = vec3(model * vec4(vertexPosition, 1.0));
+
     gl_Position = proj*view*model*vec4(vertexPosition, 1.0); 
 	uv0 = vertex_uv0;
 }
