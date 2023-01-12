01fa398e752c9dd23f8e8075c5ccdc7ea6aa9cc7
Gerard <gqueraltferre@gmail.com>
2023-01-13 00:43:22 +0100

    refactor: visibility of Importer::Load changed to public

--- Source/FileSystem/Importers/MaterialImporter.h
+++ Source/FileSystem/Importers/MaterialImporter.h
@@ -9,10 +9,10 @@ public:
     ~MaterialImporter();
 
     void Import(const char* filePath, std::shared_ptr<ResourceMaterial> resource) override;
+    void Load(const char* fileBuffer, std::shared_ptr<ResourceMaterial>& resource) override;
 
 protected:
     uint64_t Save(const std::shared_ptr<ResourceMaterial>& resource, char*& fileBuffer, unsigned int& size) override;
-    void Load(const char* fileBuffer, std::shared_ptr<ResourceMaterial>& resource) override;
 
 private:
     char* buffer;
