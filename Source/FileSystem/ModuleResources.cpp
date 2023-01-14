608722f1712df4b06f41a733f427c045cc32ebcf
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-01-14 05:37:09 +0100

    feat: Add Mat on LoadResourceStored

--- Source/FileSystem/ModuleResources.cpp
+++ Source/FileSystem/ModuleResources.cpp
@@ -110,8 +110,6 @@ void ModuleResources::LoadResourceStored(const char* filePath)
 
 void ModuleResources::ImportResourceFromLibrary(const std::string& libraryPath)
 {
-	ResourceType type = FindTypeByPath(libraryPath);
-
 	std::string metaPath;
 	std::string fileExtension = GetFileExtension(libraryPath);
 	int posOfExtensionInPath = libraryPath.find(fileExtension);
@@ -163,6 +161,7 @@ void ModuleResources::ImportResourceFromLibrary(const std::string& libraryPath)
 				case ResourceType::Scene:
 					break;
 				case ResourceType::Material:
+					materialImporter->Load(binaryBuffer, std::dynamic_pointer_cast<ResourceMaterial>(resource));
 					break;
 				case ResourceType::SkyBox:
 					break;
