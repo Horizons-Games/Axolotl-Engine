35b64581ab9fad5076bed11ef44e68f7d9f7dd2a
David Martin Sala <dms63@alu.ua.es>
2023-01-16 00:03:15 +0100

    fix: old convert deleted

--- Source/Modules/ModuleRender.cpp
+++ Source/Modules/ModuleRender.cpp
@@ -168,13 +168,9 @@ bool ModuleRender::Start()
 	UpdateProgram();
 
 	// Import models into the scene
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/BakerHouse.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/shiba.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/fox.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/fox.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/fox.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/fox.fbx");
-	App->scene->GetLoadedScene()->ConvertIntoGameObject("Assets/Models/fox.fbx");
+	App->scene->GetLoadedScene()->ConvertModelIntoGameObject("Assets/Models/BakerHouse.fbx");
+	App->scene->GetLoadedScene()->ConvertModelIntoGameObject("Assets/Models/shiba.fbx");
+	App->scene->GetLoadedScene()->ConvertModelIntoGameObject("Assets/Models/fox.fbx");
 
 	return true;
 }
