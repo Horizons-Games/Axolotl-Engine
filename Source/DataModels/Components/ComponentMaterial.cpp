83a9c056ff89fb0695d50ffed36784bbe9adaf8d
csandra24 <csandra993@gmail.com>
2023-01-15 21:54:54 +0100

    feat[ImGui]: Create component material ImGui

--- Source/DataModels/Components/ComponentMaterial.cpp
+++ Source/DataModels/Components/ComponentMaterial.cpp
@@ -9,8 +9,12 @@
 
 #include "Resources/ResourceMaterial.h"
 #include "Resources/ResourceTexture.h"
+#include "FileSystem/Importers/TextureImporter.h"
+
+#include "../../FileSystem/UniqueID.h"
 
 #include <GL/glew.h>
+#include "imgui.h"
 
 ComponentMaterial::ComponentMaterial(bool active, GameObject* owner)
 	: Component(ComponentType::MATERIAL, active, owner, true)
@@ -23,7 +27,6 @@ ComponentMaterial::~ComponentMaterial()
 
 void ComponentMaterial::Update()
 {
-	
 }
 
 void ComponentMaterial::Draw()
@@ -31,19 +34,20 @@ void ComponentMaterial::Draw()
 	unsigned int program = App->program->GetProgram();
 	glUniform3f(glGetUniformLocation(program, "material.diffuse_color"), diffuseColor.x, diffuseColor.y, diffuseColor.z);
 	std::shared_ptr<ResourceTexture> texture = textureDiffuse.lock();
-	if (texture)
+	if (texture && hasDiffuse)
 	{
 		glUniform1i(glGetUniformLocation(program, "material.has_diffuse_map"), 1);
 		glUniform1i(glGetUniformLocation(program, "material.diffuse_map"), texture->GetGlTexture());
 		glActiveTexture(GL_TEXTURE0 + texture->GetGlTexture());
 		glBindTexture(GL_TEXTURE_2D, texture->GetGlTexture());
+
 	}
 	else
 	{
 		glUniform1i(glGetUniformLocation(program, "material.has_diffuse_map"), 0);
 	}
 	texture = textureSpecular.lock();
-	if (texture)
+	if (texture && hasSpecular)
 	{
 		glUniform1i(glGetUniformLocation(program, "material.has_specular_map"), 1);
 		glUniform1i(glGetUniformLocation(program, "material.specular_map"), texture->GetGlTexture());
@@ -65,6 +69,65 @@ void ComponentMaterial::Draw()
 
 void ComponentMaterial::Display()
 {
+	ImGui::Text("");
+	ImGui::Text("MATERIAL");
+	ImGui::Text("");
+
+	if (ImGui::Button("Load a new texture"))
+	{
+
+	}
+
+	char name[20] = "Texture";
+	ImGui::InputText("Texture Name", name, 20);
+
+	ImGui::Text("");
+
+	static float3 colorDiffuse = GetDiffuseColor();
+	ImGui::Text("Diffuse Color:"); ImGui::SameLine();
+	if (ImGui::ColorEdit3("##Diffuse Color", (float*)&colorDiffuse))
+		SetDiffuseColor(colorDiffuse);
+
+	static float3 colorSpecular = GetSpecularColor();
+	ImGui::Text("Specular Color:"); ImGui::SameLine();
+	if (ImGui::ColorEdit3("##Specular Color", (float*)&colorSpecular))
+		SetSpecularColor(colorSpecular);
+
+	ImGui::Text("");
+
+	//remove actual texture
+	static UID thisUID = UniqueID::GenerateUID();
+	char* removeButtonLabel = new char[30];
+	sprintf(removeButtonLabel, "Remove Texture %d", thisUID);   // mirar
+
+	if (ImGui::Button(removeButtonLabel))
+	{
+		hasDiffuse = !hasDiffuse;
+		hasNormal = !hasNormal;
+		hasOcclusion = !hasOcclusion;
+		hasSpecular = !hasSpecular;
+		hasShininessAlpha = !hasShininessAlpha;
+	}
+
+	ImGui::Checkbox("Use specular Alpha as shininess", &hasShininessAlpha);
+	ImGui::SliderFloat("Shininess", &shininess, 0.1f, 200.f, "%.1f", ImGuiSliderFlags_AlwaysClamp);
+	ImGui::Separator();
+
+	ImGui::Text("Diffuse Texture");
+	if (ImGui::Button("Load a diffuse texture")) {
+		ImGui::OpenPopup("Choose a texture");
+	}
+
+	ImGui::Separator();
+
+	ImGui::Text("Specular Texture");
+	if (ImGui::Button("Load a specular texture")) {
+		ImGui::OpenPopup("Choose a texture");
+	}
+
+	ImGui::Separator();
+	ImGui::Text("");
+
 }
 
 void ComponentMaterial::SetDiffuseUID(UID& diffuseUID)
