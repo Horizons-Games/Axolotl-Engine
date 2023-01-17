0f2f3a358af5d637d291f3c3b8c18fd5bc052abf
Gerard <gqueraltferre@gmail.com>
2023-01-17 03:40:38 +0100

    docs: added comment with fix to bug, though it's probably not the right way

--- Source/Modules/ModuleRender.cpp
+++ Source/Modules/ModuleRender.cpp
@@ -240,6 +240,10 @@ update_status ModuleRender::Update()
 
 	AddToRenderList(App->scene->GetSelectedGameObject());
 
+	//this line fixes the bug, but I guess the gameObjects list is supposed to only contain visible objets,
+	//so it's more efficient
+	//if I'm wrong and we can just use all the gameObjects in the scene, then just do that
+	//for (GameObject* gameObject : App->scene->GetLoadedScene()->GetSceneGameObjects())
 	for (GameObject* gameObject : gameObjects)
 	{
 		if (gameObject->IsActive())
