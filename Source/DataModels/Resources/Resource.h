523a36b3cb10d53f871c78e7605448ec24e9290a
Gerard <gqueraltferre@gmail.com>
2023-01-17 01:36:03 +0100

    feat[Resource]: created Resource::IsLoaded

--- Source/DataModels/Resources/Resource.h
+++ Source/DataModels/Resources/Resource.h
@@ -23,14 +23,19 @@ public:
 
 	virtual ResourceType GetType() const = 0;
 	UID GetUID() const;
+	//name without extension
 	const std::string& GetFileName() const;
+	//path with extension
 	const std::string& GetAssetsPath() const;
+	//path without extension (extension will always be GENERAL_BINARY_EXTENSION)
 	const std::string& GetLibraryPath() const;
 
 	void Load();
 	void Unload();
 	virtual void SaveOptions(Json& meta) = 0;
 	virtual void LoadOptions(Json& meta) = 0;
+
+	bool IsLoaded() const;
 
 protected:
 	Resource(UID resourceUID, const std::string& fileName, const std::string& assetsPath, const std::string& libraryPath);
@@ -45,6 +50,11 @@ private:
 	const std::string assetsPath;
 	const std::string libraryPath;
 };
+
+inline bool Resource::IsLoaded() const
+{
+	return this->loaded;
+}
 
 inline Resource::Resource(UID resourceUID, const std::string& fileName, const std::string& assetsPath, const std::string& libraryPath):
 	uid(resourceUID),
