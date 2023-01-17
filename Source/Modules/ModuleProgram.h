63a3084a0e381c0d3c1e788f76da0543ed941cff
david castro salazar <david.cassa@hotmail.com>
2023-01-17 22:55:50 +0100

    Feat [Skybox]: load a new shader for skybox

--- Source/Modules/ModuleProgram.h
+++ Source/Modules/ModuleProgram.h
@@ -1,11 +1,22 @@
 #pragma once
 #include "Module.h"
 
+class Program;
+
+enum class ProgramType {
+	MESHSHADER,
+	SKYBOX
+};
+
 class ModuleProgram : public Module
 {
 public:
 	ModuleProgram();
 	~ModuleProgram() override;
+
+	bool Start() override;
+
+	std::shared_ptr<Program> CreateProgram(std::string vtxShaderFileName, std::string frgShaderFileName);
 
 	bool CleanUp() override;
 
@@ -15,8 +26,18 @@ public:
 	unsigned CompileShader(unsigned type, const char* source);
 
 	const unsigned& GetProgram() const { return program; }
+	const std::shared_ptr<Program> GetProgram(ProgramType type) const;
+
 
 private:
 	unsigned program;
+	std::vector<std::shared_ptr<Program> > Programs;
+	std::string RootPath = "Assets/Shaders/";
 };
+
+inline const std::shared_ptr<Program> ModuleProgram::GetProgram(ProgramType type) const
+{
+	return Programs[(int)type];
+}
+
 
