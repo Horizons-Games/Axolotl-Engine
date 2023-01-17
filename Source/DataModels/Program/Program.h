de757ac81b2fbe22ceee9fcbac34187e5a7aae13
david castro salazar <david.cassa@hotmail.com>
2023-01-17 22:51:57 +0100

    Feat [Skybox}: Porgram add.

---
+++ Source/DataModels/Program/Program.h
@@ -0,0 +1,23 @@
+#pragma once
+class Program
+{
+
+public:
+	Program(unsigned vertexShader, unsigned fragmentShader);
+	void Activate();
+	void Deactivate();
+	void CleanUp();
+	void BindUniformFloat4x4(const char* name, const float* data, bool transpose);
+	void BindUniformFloat3(const char* name, const float* data);
+	void BindUniformFloat(const char* name, const float* data);
+	void BindUniformBool(const char* name, bool value);
+	const unsigned& GetId() const;
+
+private:
+	unsigned id;
+};
+
+inline const unsigned& Program::GetId() const
+{
+	return this->id;
+}
