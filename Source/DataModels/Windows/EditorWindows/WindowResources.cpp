4629df6ef9df44cccbb27c4c5c2559ce6eec60cf
Gerard <gqueraltferre@gmail.com>
2023-01-28 14:17:15 +0100

    feat: improvements to WindowResources
    I need to figure out how imgui tables work :)

--- Source/DataModels/Windows/EditorWindows/WindowResources.cpp
+++ Source/DataModels/Windows/EditorWindows/WindowResources.cpp
@@ -9,11 +9,21 @@ void WindowResources::DrawWindowContents()
 {
 	std::vector<UID> resourcesToDelete;
 
-	//in theory, since mapEntry is a reference to the one in the resources map,
-	//it should not increase reference count while iterating since it's not a new pointer
-	for (std::pair<const UID, std::shared_ptr<Resource> >& mapEntry : App->resources->resources)
+	if (ImGui::BeginTable("Resources", 2))
 	{
-		DrawResource(mapEntry.second, resourcesToDelete);
+		ImGui::TableSetupColumn("Unloaded", ImGuiTableColumnFlags_None);
+		ImGui::TableSetupColumn("Loaded", ImGuiTableColumnFlags_None);
+		ImGui::TableHeadersRow();
+		ImGui::TableNextRow();
+
+		//in theory, since mapEntry is a reference to the one in the resources map,
+		//it should not increase reference count while iterating since it's not a new pointer
+		for (std::pair<const UID, std::shared_ptr<Resource> >& mapEntry : App->resources->resources)
+		{
+			DrawResource(mapEntry.second, resourcesToDelete);
+		}
+
+		ImGui::EndTable();
 	}
 
 	for (UID uidToDelete : resourcesToDelete)
@@ -28,7 +38,11 @@ void WindowResources::DrawResource(const std::weak_ptr<Resource>& resource, std:
 	std::shared_ptr<Resource> asShared = resource.lock();
 	if (asShared)
 	{
+		ImGui::TableNextRow();
+		ImGui::TableSetColumnIndex(asShared->IsLoaded());
 		std::string uidString = std::to_string(asShared->GetUID());
+
+		ImGui::BeginGroup();
 
 		ImGui::TextUnformatted(("UID: " + uidString).c_str());
 		ImGui::TextUnformatted(("Name: " + asShared->GetFileName()).c_str());
@@ -38,24 +52,36 @@ void WindowResources::DrawResource(const std::weak_ptr<Resource>& resource, std:
 		//a bit yucky I guess
 		ImGui::TextUnformatted(("Resource type: " + App->resources->GetNameOfType(asShared->GetType())).c_str());
 		ImGui::TextUnformatted(("Reference count (should be 1): " + std::to_string(referenceCountBeforeLock)).c_str());
-		
-		ImGui::BeginDisabled();
-		bool isLoaded = asShared->IsLoaded();
-		ImGui::Checkbox(("Is loaded##" + uidString).c_str(), &isLoaded);
-		ImGui::EndDisabled();
 
-		if (ImGui::Button(("Load resource##" + uidString).c_str()))
-		{
-			asShared->Load();
-		}
-		if (ImGui::Button(("Unload resource##" + uidString).c_str()))
-		{
-			asShared->Unload();
-		}
-		if (ImGui::Button(("Delete resource##" + uidString).c_str()))
-		{
-			resourcesToDelete.push_back(asShared->GetUID());
-		}
 		ImGui::Separator();
+
+		ImGui::EndGroup();
+
+		if (ImGui::IsItemClicked(ImGuiMouseButton_Right))
+		{
+			ImGui::OpenPopup(("Manage resource##" + uidString).c_str());
+		}
+
+		if (ImGui::BeginPopup(("Manage resource##" + uidString).c_str()))
+		{
+			if (!asShared->IsLoaded() && ImGui::MenuItem(("Load resource##" + uidString).c_str()))
+			{
+				asShared->Load();
+			}
+			if (asShared->IsLoaded() && ImGui::MenuItem(("Unload resource##" + uidString).c_str()))
+			{
+				asShared->Unload();
+			}
+			if (ImGui::MenuItem(("Delete resource##" + uidString).c_str()))
+			{
+				resourcesToDelete.push_back(asShared->GetUID());
+			}
+			ImGui::EndPopup();
+		}
+
+		if (ImGui::IsItemClicked(ImGuiMouseButton_Left))
+		{
+			//TODO: resource options
+		}
 	}
 }
