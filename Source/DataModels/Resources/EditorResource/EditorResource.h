98a7d0fcfdc49cfb797a45c9cc3876998dd8d7c6
gerard-queralt <gqueraltferre@gmail.com>
2023-02-21 19:25:46 +0100

    feat: created EditorResource

---
+++ Source/DataModels/Resources/EditorResource/EditorResource.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+
+#include "Resources/Resource.h"
+
+//ideally this template should be safeguarded like in GameObject::GetComponentByType
+//but I had some issues and I don't want to waste time on it
+template<typename R>
+class EditorResource : public R
+{
+	EditorResource(UID resourceUID,
+				   const std::string& fileName,
+				   const std::string& assetsPath,
+				   const std::string& libraryPath);
+	virtual ~EditorResource() override;
+
+public:
+	bool ToDelete() const;
+
+	void MarkToDelete();
+
+private:
+	bool toDelete;
+};
+
+template<typename R>
+inline EditorResource<R>::EditorResource(UID resourceUID,
+										 const std::string& fileName,
+										 const std::string& assetsPath,
+										 const std::string& libraryPath) :
+	R(resourceUID, fileName, assetsPath, libraryPath),
+	toDelete(false)
+{
+}
+
+template<typename R>
+inline EditorResource<R>::~EditorResource()
+{
+}
+
+template<typename R>
+inline bool EditorResource<R>::ToDelete() const
+{
+	return toDelete;
+}
+
+template<typename R>
+inline void EditorResource<R>::MarkToDelete()
+{
+	toDelete = true;
+}
