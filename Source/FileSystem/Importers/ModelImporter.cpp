578e247e96b2ee32583d003b07495e3348b97509
Oriol Murcia Catalan <45937802+UriMurcia@users.noreply.github.com>
2023-03-06 21:03:13 +0100

    fix[tangents]: assimp tangent calculations
    Assimp flag added to calculate tangents all the time, fixing the bug that made the objects turn black when had normal maps on them

--- Source/FileSystem/Importers/ModelImporter.cpp
+++ Source/FileSystem/Importers/ModelImporter.cpp
@@ -33,7 +33,7 @@ void ModelImporter::Import(const char* filePath, std::shared_ptr<ResourceModel> 
 
 	ENGINE_LOG("Import Model from %s", filePath);
 
-	const aiScene* scene = aiImportFile(filePath, aiProcess_Triangulate);
+	const aiScene* scene = aiImportFile(filePath, aiProcess_Triangulate | aiProcess_CalcTangentSpace);
 	if (scene)
 	{
 		ImportMaterials(scene, filePath, resource);
