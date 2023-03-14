f276d7e7cd6892b151409199bf9b35e67ed1f980
David Martin Sala <dms63@alu.ua.es>
2023-03-14 18:32:18 +0100

    fix[Camera]: camera type corrected

--- Source/DataModels/Camera/CameraGameObject.cpp
+++ Source/DataModels/Camera/CameraGameObject.cpp
@@ -2,12 +2,12 @@
 
 
 CameraGameObject::CameraGameObject()
-	: Camera(CameraType::C_ENGINE)
+	: Camera(CameraType::C_GAMEOBJECT)
 {
 };
 
 CameraGameObject::CameraGameObject(const std::unique_ptr<Camera>& camera)
-	: Camera(camera, CameraType::C_ENGINE)
+	: Camera(camera, CameraType::C_GAMEOBJECT)
 {
 }
 
