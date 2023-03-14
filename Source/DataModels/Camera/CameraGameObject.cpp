155f98115db79718538ce1ef353cc134635d4ae0
David Martin Sala <dms63@alu.ua.es>
2023-03-14 11:46:41 +0100

    feat[CameraGameObject]: Camera GameObject Creation

--- Source/DataModels/Camera/CameraGameObject.cpp
+++ Source/DataModels/Camera/CameraGameObject.cpp
@@ -1,1 +1,22 @@
 #include "CameraGameObject.h"
+
+
+CameraGameObject::CameraGameObject()
+	: Camera(CameraType::C_ENGINE)
+{
+};
+
+CameraGameObject::CameraGameObject(const std::unique_ptr<Camera>& camera)
+	: Camera(camera, CameraType::C_ENGINE)
+{
+}
+
+CameraGameObject::~CameraGameObject()
+{
+};
+
+
+bool CameraGameObject::Update()
+{
+	return true;
+}
