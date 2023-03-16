8aae22406e84d6a1921fed9bf8b146869a1e7168
AlexisDieu <alexisdieu3@gmail.com>
2023-03-17 00:02:16 +0100

    fix : delete usless line

--- Source/DataModels/Batch/GeometryBatch.cpp
+++ Source/DataModels/Batch/GeometryBatch.cpp
@@ -173,8 +173,6 @@ void GeometryBatch::CreateVAO()
 	// Instance indices
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ARRAY_BUFFER, ebo);
-	glVertexAttribPointer(4, 1, GL_FLOAT, GL_FALSE, 1 * sizeof(float), static_cast<void*>(nullptr));
-	glEnableVertexAttribArray(1);
 
 	glGenBuffers(1, &indirectBuffer);
 	glBindBuffer(GL_DRAW_INDIRECT_BUFFER, indirectBuffer);
