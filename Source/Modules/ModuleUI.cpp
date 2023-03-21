259548785e7524337949654fbc4d4082010bf6b6
David Martin Sala <dms63@alu.ua.es>
2023-03-21 20:47:38 +0100

    Feat[ModuleUI]: basic implementation

---
+++ Source/Modules/ModuleUI.cpp
@@ -0,0 +1,52 @@
+#include "ModuleUI.h"
+
+#include "ModuleScene.h"
+
+#include "Scene/Scene.h"
+
+#include "Application.h"
+
+ModuleUI::ModuleUI() 
+{
+};
+
+ModuleUI::~ModuleUI() {
+};
+
+bool ModuleUI::Init()
+{
+	return true;
+}
+
+bool ModuleUI::Start()
+{
+	return true;
+}
+
+update_status ModuleUI::Update()
+{
+	std::vector<GameObject*> canvasScene = App->scene->GetLoadedScene()->GetSceneCanvas();
+
+	for (GameObject* canvas : canvasScene)
+	{
+		for (GameObject* children : canvas->GetChildren())
+		{
+
+			DrawChildren(children);
+		}
+	}
+
+	return update_status::UPDATE_CONTINUE;
+}
+
+void ModuleUI::DrawChildren(GameObject* gameObject)
+{
+	if (gameObject->IsEnabled())
+	{
+		gameObject->Draw();
+		for (GameObject* children : gameObject->GetChildren())
+		{
+			DrawChildren(children);
+		}
+	}
+}
---
+++ Source/Modules/ModuleUI.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Module.h"
+
+
+class GameObject;
+
+class ModuleUI : public Module
+{
+public:
+	ModuleUI();
+	~ModuleUI();
+
+	bool Init();
+	bool Start();
+
+	update_status Update();
+
+	void DrawChildren(GameObject* gameObject);
+};
+
