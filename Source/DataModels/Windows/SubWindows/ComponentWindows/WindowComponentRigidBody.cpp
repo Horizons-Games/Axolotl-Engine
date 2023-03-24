9249f5bcaf9ed2c58e028bcf987867b08997f813
rodrigo samayoa <toti1998sama@gmail.com>
2023-03-24 14:18:17 +0100

    Feat: adding imgui to WindowComponentRigidBody

--- Source/DataModels/Windows/SubWindows/ComponentWindows/WindowComponentRigidBody.cpp
+++ Source/DataModels/Windows/SubWindows/ComponentWindows/WindowComponentRigidBody.cpp
@@ -14,5 +14,21 @@ WindowComponentRigidBody::~WindowComponentRigidBody()
 void WindowComponentRigidBody::DrawWindowContents()
 {
 	DrawEnableAndDeleteComponent();
-	ImGui::Text("");
+
+	ImGui::Text(""); //used to ignore the ImGui::SameLine called in DrawEnableAndDeleteComponent
+	
+	ComponentRigidBody* asRigidBody = static_cast<ComponentRigidBody*>(component);
+
+	if (asRigidBody)
+	{
+		ImGui::Text("");
+
+		bool isKinematic = asRigidBody->GetIsKinematic();
+
+		ImGui::Text("Is Kinematic"); ImGui::SameLine();
+		if (ImGui::Checkbox("##Is Kinematic", &isKinematic))
+		{
+			asRigidBody->SetIsKinematic(isKinematic);
+		}
+	}
 }
--- Source/DataModels/Windows/SubWindows/ComponentWindows/WindowComponentRigidBody.h
+++ Source/DataModels/Windows/SubWindows/ComponentWindows/WindowComponentRigidBody.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "ComponentWindow.h"
 
 class ComponentRigidBody;
