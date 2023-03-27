a6ec18a67fbf34600915dadcdf11e975c73386ba
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-03-27 18:41:54 +0200

    feat: Orthographic camera setting before UI Render

--- Source/Modules/ModuleUI.cpp
+++ Source/Modules/ModuleUI.cpp
@@ -5,6 +5,10 @@
 #include "Scene/Scene.h"
 #include "Components/ComponentCanvas.h"
 #include "Application.h"
+#include "ModuleWindow.h"
+#include "ModuleCamera.h"
+
+#include "GL/glew.h"
 
 ModuleUI::ModuleUI() 
 {
@@ -26,6 +30,17 @@ bool ModuleUI::Start()
 update_status ModuleUI::Update()
 {
 	std::vector<GameObject*> canvasScene = App->scene->GetLoadedScene()->GetSceneCanvas();
+	int width, height;
+	SDL_GetWindowSize(App->window->GetWindow(), &width, &height);
+	
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, width, height, 0, 1, -1);
+	glMatrixMode(GL_MODELVIEW);
+
+	App->camera->GetCamera()->GetFrustum()->SetOrthographic(width, height);
+
+	glDisable(GL_DEPTH_TEST);
 
 	for (GameObject* canvas : canvasScene)
 	{
@@ -37,6 +52,15 @@ update_status ModuleUI::Update()
 			}
 		}
 	}
+
+	glEnable(GL_DEPTH_TEST);
+	App->camera->GetCamera()->GetFrustum()->
+		SetHorizontalFovAndAspectRatio(math::DegToRad(90), App->camera->GetCamera()->GetAspectRatio());
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1, 1, -1, 1, -1, 1);
+	glMatrixMode(GL_MODELVIEW);
 
 	return update_status::UPDATE_CONTINUE;
 }
