20f37eb4a406ff69853a0a2f20897130579cd117
Eric <eric@dacal.es>
2023-04-02 19:22:23 +0200

    fix: erase unused code in ComponentScript constructor

--- Source/DataModels/Components/ComponentScript.cpp
+++ Source/DataModels/Components/ComponentScript.cpp
@@ -3,10 +3,6 @@
 
 ComponentScript::ComponentScript(bool active, GameObject* owner): Component(ComponentType::SCRIPT, active, owner, false)
 {
-	camera = std::make_unique <CameraGameObject>();
-	camera->Init();
-	camera->SetViewPlaneDistance(DEFAULT_GAMEOBJECT_FRUSTUM_DISTANCE);
-	Update();
 }
 
 
