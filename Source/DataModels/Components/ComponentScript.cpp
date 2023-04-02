2af2a9f75315c6f41b49599ceffa52bb38ca1b2c
Eric <eric@dacal.es>
2023-04-02 19:50:17 +0200

    feat: Implementation of Script Component script to can load and unload DLLs as script

--- Source/DataModels/Components/ComponentScript.cpp
+++ Source/DataModels/Components/ComponentScript.cpp
@@ -1,18 +1,37 @@
 #include "ComponentScript.h"
+#include "Application.h"
 
 
-ComponentScript::ComponentScript(bool active, GameObject* owner): Component(ComponentType::SCRIPT, active, owner, false)
-{
+ComponentScript::ComponentScript(bool active, GameObject* owner, const char* path): Component(ComponentType::SCRIPT, active, owner, false){
+	this->path = path;
 }
 
+ComponentScript::~ComponentScript() {
 
+}
+
+void ComponentScript::LoadLibraryScript(){
+	this->library = LoadLibraryA(this->path);
+	this->scriptInit = (initScript)GetProcAddress(this->library, "Init()");
+	if (this->scriptInit == 0) ENGINE_LOG("Error loading script");
+	this->scriptUpdate = (updateScript)GetProcAddress(this->library, "Update()");
+	if (this->scriptUpdate == 0) ENGINE_LOG("Error loading script");
+}
+
+void ComponentScript::FreeLibraryScript() {
+	FreeLibrary(this->library);
+	this->scriptInit = nullptr;
+	this->scriptUpdate = nullptr;
+}
 
 void ComponentScript::Init()
 {
-	
+	this->LoadLibraryScript();
+	this->scriptInit(this->owner);
 }
 
 
 void ComponentScript::Update()
 {
+	this->scriptUpdate(this->owner);
 }
--- Source/DataModels/Components/ComponentScript.h
+++ Source/DataModels/Components/ComponentScript.h
@@ -1,17 +1,27 @@
 #pragma once
 #include "Component.h"
+#include <memory>
+#include <windows.h>
+
 
 #define COMPONENT_SCRIPT "Script"
+
+class Application;
 
 class ComponentScript : public Component
 {
 public:
-	ComponentScript(bool active, GameObject* owner);
+	ComponentScript(bool active, GameObject* owner, const char* path);
 	~ComponentScript() override;
 	void Init() override;
 	void Update() override;
 private:
-	std::string path;
-	void LoadLibrary();
-	void FreeLibrary();
+	typedef void* (*initScript)(GameObject* owner);
+	typedef void* (*updateScript)(GameObject* owner);
+	const char* path;
+	void LoadLibraryScript();
+	void FreeLibraryScript();
+	initScript scriptInit;
+	updateScript scriptUpdate;
+	HINSTANCE library;
 };
