01ec0813d6786bf3cf1e78ac1010dfd614b296c9
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2023-04-07 18:35:18 +0200

    fix: readd ComponentAnimation

---
+++ Source/DataModels/Components/ComponentAnimation.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Components/Component.h"
+
+class ComponentAnimation : public Component
+{
+public:
+	ComponentAnimation(const bool active, GameObject* owner);
+	~ComponentAnimation() override;
+
+	void Update() override;
+	void Draw() override;
+
+	void SaveOptions(Json& meta) override;
+	void LoadOptions(Json& meta) override;
+
+private:
+};
