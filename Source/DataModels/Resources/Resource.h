e8eef85cc65420717e3199b4e5772a2a34ea9b16
Eric <eric@dacal.es>
2023-04-12 20:00:34 +0200

    feat: Create ResourceScript as a template

--- Source/DataModels/Resources/Resource.h
+++ Source/DataModels/Resources/Resource.h
@@ -13,6 +13,7 @@ enum class ResourceType
 	Scene,
 	Material,
 	SkyBox,
+	Script
 };
 
 class Resource
---
+++ Source/DataModels/Resources/ResourceScript.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Resource.h"
+#include "IScript.h"
+#include <memory>
+
+class ResourceScript;
+
+class ResourceScript : virtual public Resource
+{
+public:
+	ResourceScript(UID resourceUID,
+		const std::string& fileName,
+		const std::string& assetsPath,
+		const std::string& libraryPath);
+	virtual ~ResourceScript() override;
+
+	ResourceType GetType() const override;
+
+	const char* GetScriptPath();
+
+	void SaveImporterOptions(Json& meta) override {};
+	void LoadImporterOptions(Json& meta) override {};
+
+	void SaveLoadOptions(Json& meta) override {};
+	void LoadLoadOptions(Json& meta) override {};
+private:
+	const char* scriptPath;
+};
+
+inline ResourceType ResourceScript::GetType() const
+{
+	return ResourceType::Script;
+}
+
+inline const char* ResourceScript::GetScriptPath() 
+{
+	return scriptPath;
+}
