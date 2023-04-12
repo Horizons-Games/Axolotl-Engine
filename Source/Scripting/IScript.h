e6e67fb77b4ede8eb35baf66b18fad1d94b58178
Eric <eric@dacal.es>
2023-04-12 19:08:41 +0200

    feat: Implementation of a base interface for scripts

---
+++ Source/Scripting/IScript.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef ISCRIPT_INCLUDED
+#define ISCRIPT_INCLUDED
+
+#include "IObject.h"
+
+class GameObject;
+
+struct IScript : public IObject
+{
+	virtual void Init() = 0;
+	virtual void Start() = 0;
+	virtual void PreUpdate() = 0;
+	virtual void Update(float deltaTime) = 0;
+	virtual void PostUpdate() = 0;
+};
+
+#endif // ISCRIPT_INCLUDED
