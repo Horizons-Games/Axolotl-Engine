389915d81c45cd91763235f11d067a3e494e55a2
david castro salazar <david.cassa@hotmail.com>
2023-04-14 17:15:48 +0200

    Fix: problem when a resource is unload
    Co-Authored-By: Pablo Cebollada Hernández <62140600+PaablooCH@users.noreply.github.com>

--- Source/DataModels/Components/ComponentMaterial.cpp
+++ Source/DataModels/Components/ComponentMaterial.cpp
@@ -27,7 +27,7 @@ ComponentMaterial::ComponentMaterial(bool active, GameObject* owner)
 }
 
 ComponentMaterial::ComponentMaterial(const ComponentMaterial& componentMaterial) :
-	Component(componentMaterial), material(componentMaterial.GetMaterial())
+	Component(componentMaterial), material(componentMaterial.material)
 {
 }
 
