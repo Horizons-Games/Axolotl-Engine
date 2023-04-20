ab99fee7ca361474cb835df9e36dc4d49d4b85cf
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-04-20 03:12:17 +0200

    feat: Ad WindowResourceInput

---
+++ Source/DataModels/Windows/EditorWindows/ImporterWindows/WindowResourceInput.cpp
@@ -0,0 +1,25 @@
+#include "WindowResourceInput.h"
+
+#include "Application.h"
+#include "FileSystem/ModuleResources.h"
+#include "ModuleEditor.h"
+#include "DataModels/Resources/Resource.h"
+
+WindowResourceInput::WindowResourceInput()
+{
+	dialogName = "Select Texture";
+	title = "Load Resource";
+	filters = "Resource Files (*.png){.png}";
+	startPath = "Assets/Textures";
+}
+
+WindowResourceInput::~WindowResourceInput()
+{
+}
+
+void WindowResourceInput::DoThisIfOk()
+{
+	std::string filePath = std::string(fileDialogImporter.GetFilePathName());
+	std::shared_ptr<Resource> resource = App->resources->RequestResource<Resource>(filePath);
+	App->editor->SetResourceOnStateMachineEditor(resource);
+}
