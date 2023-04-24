c1aeb7f93c832b68fd4f27ceae544116b10cd50d
Oriol Murcia Catalan <oriolmurciacatalan@gmail.com>
2023-04-24 21:06:51 +0200

    feat: new audios

--- Source/Auxiliar/Audio/AudioData.h
+++ Source/Auxiliar/Audio/AudioData.h
@@ -3,24 +3,43 @@
 namespace audio
 {
 	//SFX
+
+	//NPC
 	static const wchar_t* SFX_DRON_SHOT_01 = L"sfx_dron_shot_01";
 
 	//Ambient
 	static const wchar_t* SFX_AMBIENT_CROWD_TALKING_01 = L"sfx_ambient_crowd_talking_01";
 	static const wchar_t* CANTINA_MUSIC = L"cantina_music";
 	static const wchar_t* AUXILIAR_SEWERS_MUSIC = L"sewersMusic";
+	static const wchar_t* STOP_AMBIENT_CANTINA = L"ambient_cantina_stop";
+
+	//Player
+	static const wchar_t* SFX_PLAYER_FOOTSTEPS_WALK = L"sfx_footsteps_walk_01";
+	static const wchar_t* SFX_PLAYER_FOOTSTEPS_WALK_STOP = L"sfx_footsteps_walk_stop";
+	static const wchar_t* SFX_PLAYER_FOOTSTEPS_RUN = L"sfx_footsteps_run_01";
+	static const wchar_t* SFX_PLAYER_FOOTSTEPS_RUN_STOP = L"sfx_footsteps_run_stop";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_CLASH = L"sfx_lightsaber_clash";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_OPEN = L"sfx_lightsaber_open_01";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_SWING = L"sfx_lightsaber_swing";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_CLOSE = L"sfx_lightsaber_close_01";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_HUM = L"sfx_lightsaber_hum_01";
+	static const wchar_t* SFX_PLAYER_LIGHTSABER_HUM_STOP = L"sfx_lightsaber_hum_stop";
+
+	//UI
+	static const wchar_t* SFX_UI_BUTTON_BACK = L"sfx_button_back_01";
+	static const wchar_t* SFX_UI_BUTTON_PRESS = L"sfx_button_press_01";
+	static const wchar_t* SFX_UI_BUTTON_HOVER = L"sfx_button_hover_01";
 
 
-	//Music
+	//MUSIC
 	static const wchar_t* PLAY_MUSIC = L"Music";
-
+	static const wchar_t* STOP_MUSIC = L"StopMusic";
 
 	//Music Switch group
 	static const wchar_t* SWITCH_GROUP_MUSIC_GAMEPLAY = L"Gameplay";
 	//Music Switch IDs
 	static const wchar_t* SWITCH_ID_MUSIC_EXPLORATION = L"Exploration";
 	static const wchar_t* SWITCH_ID_MUSIC_COMBAT = L"Combat";
-
 
 	//State group
 	static const wchar_t* STATE_GROUP_LIFE = L"PlayerLife";
