0aee612c909e00dc707476efb92aa5ae5e714736
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-05-02 23:45:53 +0200

    refactor: const reference

--- Source/DataModels/Resources/ResourceAnimation.cpp
+++ Source/DataModels/Resources/ResourceAnimation.cpp
@@ -1,17 +1,11 @@
 #include "ResourceAnimation.h"
 
 ResourceAnimation::ResourceAnimation(UID resourceId, const std::string& fileName, const std::string& assetsPath, 
-	const std::string& libraryPath) : Resource(resourceId, fileName, assetsPath, libraryPath),
-	channels(NULL), duration(0.0f)
+	const std::string& libraryPath) : Resource(resourceId, fileName, assetsPath, libraryPath), duration(0.0f)
 {
 }
 
 ResourceAnimation::~ResourceAnimation()
-{
-	Unload();
-}
-
-void ResourceAnimation::InternalUnload()
 {
 	for (auto it = channels.begin(); it != channels.end(); ++it)
 	{
@@ -19,4 +13,8 @@ void ResourceAnimation::InternalUnload()
 	}
 
 	channels.clear();
+}
+
+void ResourceAnimation::InternalUnload()
+{
 }
