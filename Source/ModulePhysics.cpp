0c6682e10608741fa72cf061a06974228189b42a
Xavier Llubes Cano <xavierllubes11@gmail.com>
2023-05-03 19:39:32 +0200

    feat: change library to correct ones + ModulePhysics "HelloWorld" initialization

--- Source/ModulePhysics.cpp
+++ Source/ModulePhysics.cpp
@@ -11,11 +11,28 @@ ModulePhysics::~ModulePhysics()
 
 bool ModulePhysics::Init()
 {
+	//collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+
+	///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+	btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+	///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+	btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
+
+	///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+	btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+	btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+
+	dynamicsWorld->setGravity(btVector3(0, -10, 0));
+
     return true;
 }
 
 update_status ModulePhysics::Update()
 {
+    
     return update_status::UPDATE_CONTINUE;
 }
 
