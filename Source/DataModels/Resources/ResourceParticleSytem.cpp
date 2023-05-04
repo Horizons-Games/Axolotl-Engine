93cb277707183063bfebd68657d57ad9045622f5
Miquel Prieto Moliné <mikipmoline@gmail.com>
2023-05-05 00:14:03 +0200

    chore[particles]: filter config

--- Source/DataModels/Resources/ResourceParticleSytem.cpp
+++ Source/DataModels/Resources/ResourceParticleSytem.cpp
@@ -1,12 +1,12 @@
 #include "ResourceParticle.h"
 
-ResourceParticle::ResourceParticle(UID resourceUID, const std::string& fileName, const std::string& assetsPath, 
+ResourceParticleSystem::ResourceParticleSystem(UID resourceUID, const std::string& fileName, const std::string& assetsPath, 
 	const std::string& libraryPath) :
 	Resource(resourceUID, fileName, assetsPath, libraryPath)
 {
 }
 
-ResourceParticle::~ResourceParticle()
+ResourceParticleSystem::~ResourceParticleSystem()
 {
 	emitters.clear();
 }
