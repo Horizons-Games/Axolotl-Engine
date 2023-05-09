d8c857a78853fd221a6005b2148702fa0eac6608
Oriol Sors <oriolsors@gmail.com>
2023-05-09 19:01:49 +0200

    feat: ModulePhysics collision management

--- Source/Modules/ModulePhysics.cpp
+++ Source/Modules/ModulePhysics.cpp
@@ -1,11 +1,25 @@
 #include "ModulePhysics.h"
 
+#include "Bullet/btBulletDynamicsCommon.h"
+#include "Application.h"
+
+#include "GameObject/GameObject.h"
+#include "Components/ComponentRigidBody.h"
+#include "Components/ComponentTransform.h"
+
 ModulePhysics::ModulePhysics()
 {
 }
 
 ModulePhysics::~ModulePhysics()
 {
+}
+
+void NearCallback(btBroadphasePair& collisionPair, btCollisionDispatcher& dispatcher, btDispatcherInfo& dispatchInfo)
+{
+	// Do your collision logic here, here you can filter which objects collide with each other if you have multiple collision layers
+	// Only dispatch the Bullet collision information if you want the physics to continue    
+	dispatcher.defaultNearCallback(collisionPair, dispatcher, dispatchInfo);
 }
 
 bool ModulePhysics::Init()
@@ -27,4 +41,133 @@ bool ModulePhysics::Init()
 	dynamicsWorld->setGravity(btVector3(0, -10, 0));
 
     return true;
+}
+
+bool ModulePhysics::CleanUp()
+{
+	collisions.clear();
+	rigidBodyComponents.clear();
+
+	return true;
+}
+
+void ModulePhysics::Reset()
+{
+	CleanUp();
+	Init();
+}
+
+update_status ModulePhysics::PreUpdate()
+{
+	dynamicsWorld->stepSimulation(App->GetDeltaTime(), 10);
+	ManageCollisions();
+	return update_status::UPDATE_CONTINUE;
+}
+
+
+void ModulePhysics::ManageCollisions()
+{
+
+    struct ContactResultCallback : public btCollisionWorld::ContactResultCallback
+    {
+        bool collisionDetected;
+        const btCollisionObject* otherRigidBody;
+
+        ContactResultCallback() : collisionDetected(false), otherRigidBody(nullptr) {}
+
+        virtual btScalar addSingleResult(btManifoldPoint& cp,
+            const btCollisionObjectWrapper* colObj0Wrap,
+            int partId0,
+            int index0,
+            const btCollisionObjectWrapper* colObj1Wrap,
+            int partId1,
+            int index1)
+        {
+            collisionDetected = true;
+            otherRigidBody = colObj1Wrap->getCollisionObject();
+            return 0;
+        }
+    };
+
+
+    btCollisionObjectArray collisionArray = dynamicsWorld->getCollisionObjectArray();
+    for (int i = 0; i < collisionArray.size(); i++)
+    {
+        btCollisionObject* obj = collisionArray[i];
+
+        ContactResultCallback result;
+        dynamicsWorld->contactTest(obj, result);
+
+        if (result.collisionDetected)
+        {
+            if (obj->getUserPointer() != nullptr)
+            {
+                ComponentRigidBody* rb = static_cast<ComponentRigidBody*>(obj->getUserPointer());
+                if (rb != nullptr)
+                {
+                    ComponentRigidBody* other = static_cast<ComponentRigidBody*>(result.otherRigidBody->getUserPointer());
+                    assert(rb && other);
+                    uint64_t i1 = rb->GetID();
+                    i1 = i1 << 32;
+                    uint64_t i2 = other->GetID();
+                    //key is the combination of the two indexes in the high 32 bits store the first index and in the low 32 bits store the second index
+                    uint64_t key = i1 | i2;
+                    if (collisions.find(key) == collisions.end())
+                    {
+                        rb->OnCollisionEnter(other);
+                    }
+                    else
+                    {
+                        rb->OnCollisionStay(other);
+                    }
+                    collisions.insert(key);
+                }
+            }
+        }
+    }
+
+    //iterate over collision and remove the ones which does not have contact
+    for (auto it = collisions.begin(); it != collisions.end();)
+    {
+        uint32_t i1 = *it >> 32;
+        uint32_t i2 = *it & 0x00000000FFFFFFFF; //remove high 32 bits
+        ComponentRigidBody* rb = static_cast<ComponentRigidBody*>(rigidBodyComponents[i1]);
+        ComponentRigidBody* other = static_cast<ComponentRigidBody*>(rigidBodyComponents[i2]);
+        if (!rb || !other)
+        {
+            it = collisions.erase(it);
+            continue;
+        }
+
+        //check if the collision still exists
+        ContactResultCallback result;
+        dynamicsWorld->contactPairTest(rb->GetRigidBody(), other->GetRigidBody(), result);
+        if (!result.collisionDetected)
+        {
+            rb->OnCollisionExit(other);
+            it = collisions.erase(it);
+        }
+        else
+        {
+            ++it;
+        }
+
+    }
+
+}
+
+void ModulePhysics::AddRigidBody(ComponentRigidBody* rb, btRigidBody* body)
+{
+    dynamicsWorld->addRigidBody(body);
+    rigidBodyComponents[rb->GetID()] = rb;
+}
+
+void ModulePhysics::RemoveRigidBody(ComponentRigidBody* rb, btRigidBody* body)
+{
+    if (dynamicsWorld)
+    {
+        dynamicsWorld->removeCollisionObject(body);
+        dynamicsWorld->removeRigidBody(body);
+    }
+    rigidBodyComponents.erase(rb->GetID());
 }
--- Source/Modules/ModulePhysics.h
+++ Source/Modules/ModulePhysics.h
@@ -2,14 +2,31 @@
 
 #include "Module.h"
 #include "Bullet/btBulletDynamicsCommon.h"
+#include <unordered_map>
+#include <unordered_set>
+
+class btRigidBody;
+struct btBroadphasePair;
+class btCollisionDispatcher;
+struct btDispatcherInfo;
+class ComponentRigidBody;
 
 class ModulePhysics : public Module
 {
 public:
-	ModulePhysics();
-	~ModulePhysics() override;
-
 	bool Init() override;
+	update_status PreUpdate() override;
+
+	void ManageCollisions();
+
+	void AddRigidBody(ComponentRigidBody* rb, btRigidBody* body);
+	void RemoveRigidBody(ComponentRigidBody* rb, btRigidBody* body);
+
+	uint64_t GetAmountOfRigidBodies() const { return rigidBodyComponents.size(); }
+
+	bool CleanUp() override;
+
+	void Reset();
 
 private:
 
@@ -18,4 +35,7 @@ private:
 	std::unique_ptr<btBroadphaseInterface> overlappingPairCache;
 	std::unique_ptr<btSequentialImpulseConstraintSolver> solver;
 	std::unique_ptr<btDiscreteDynamicsWorld> dynamicsWorld;
+
+	std::unordered_set<uint64_t> collisions;
+	std::unordered_map<uint32_t, ComponentRigidBody*> rigidBodyComponents;
 };
