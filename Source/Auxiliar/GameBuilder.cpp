b2594000292d5049990c62d0024fad61a7673339
Gerard <gqueraltferre@gmail.com>
2023-05-10 02:57:52 +0200

    fix: regenerated scenes, added meta, fixed copying into Lib

--- Source/Auxiliar/GameBuilder.cpp
+++ Source/Auxiliar/GameBuilder.cpp
@@ -39,27 +39,37 @@ namespace builder
 			ENGINE_LOG("Done compiling!");
 		}
 
-		void CreateZip()
+		void CopyFolderInLib(const std::string& sourcePath, const std::string& destinationInsideLib)
 		{
 			const ModuleFileSystem* fileSystem = App->GetModule<ModuleFileSystem>();
 
+			std::string fullLibPath = "Lib/" + destinationInsideLib;
+			if (!fileSystem->Exists(fullLibPath.c_str()))
 			{
-				std::vector<std::string> scenes = fileSystem->ListFiles(SCENE_PATH);
-				for (const std::string& sceneFileName : scenes)
-				{
-					fileSystem->Copy(SCENE_PATH + sceneFileName, "Lib/Scenes/" + sceneFileName);
-				}
-			}
-			{
-				std::vector<std::string> shaders = fileSystem->ListFiles("Source/Shaders/");
-				fileSystem->CreateDirectory("Lib/Shaders/");
-				for (const std::string& shader : shaders)
-				{
-					fileSystem->Copy("Source/Shaders/" + shader, "Lib/Shaders/" + shader);
-				}
+				fileSystem->CreateDirectory(fullLibPath.c_str());
 			}
 
-			fileSystem->ZipLibFolder();
+			std::vector<std::string> items = fileSystem->ListFiles(sourcePath.c_str());
+			for (const std::string& itemName : items)
+			{
+				std::string itemAndPath = sourcePath + itemName;
+				if (fileSystem->IsDirectory(itemAndPath.c_str()))
+				{
+					CopyFolderInLib(itemAndPath + "/", destinationInsideLib + itemName + "/");
+				}
+				else
+				{
+					fileSystem->Copy(itemAndPath, fullLibPath + itemName);
+				}
+			}
+		}
+
+		void CreateZip()
+		{
+			CopyFolderInLib(SCENE_PATH, "Scenes/");
+			CopyFolderInLib("Source/Shaders/", "Shaders/");
+
+			App->GetModule<ModuleFileSystem>()->ZipLibFolder();
 
 			ENGINE_LOG("Done creating ZIP!");
 		}
@@ -100,12 +110,12 @@ namespace builder
 	}
 
 	bool Compiling()
-{
+	{
 		return compileThread.valid() && !compileThread._Is_ready();
 	}
 
 	bool Zipping()
-{
+	{
 		return zipThread.valid() && !zipThread._Is_ready();
 	}
 
