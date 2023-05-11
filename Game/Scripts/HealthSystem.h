8403e5d92b4d6dbdfa9584b9477234562cf0059d
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-05-11 21:04:54 +0200

    chore: delete duplicate HealthSystem script

--- Game/Scripts/HealthSystem.h
+++ Game/Scripts/HealthSystem.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "Scripting\Script.h"
+
+class HealthSystem : public Script
+{
+public:
+	HealthSystem();
+	~HealthSystem() override = default;
+
+	void Start() override;
+	void Update(float deltaTime) override;
+
+	float GetHealth() const;
+	void SetHealth(float health);
+
+	void TakeDamage(float damage);
+	void HealLife(float amountHealed);
+
+
+private:
+	float health;
+	float maxHealth;
+};
