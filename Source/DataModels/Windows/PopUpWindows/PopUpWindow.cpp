7138ab2afdd8537cc2ace21bd7d4175c813e1db3
Gerard <gqueraltferre@gmail.com>
2023-05-12 13:49:34 +0200

    style: column limit

--- Source/DataModels/Windows/PopUpWindows/PopUpWindow.cpp
+++ Source/DataModels/Windows/PopUpWindows/PopUpWindow.cpp
@@ -17,7 +17,10 @@ void PopUpWindow::Draw(bool& enabled)
 		ImGui::OpenPopup(name.c_str());
 		// Center the PopUp
 		ImGuiIO& io = ImGui::GetIO();
-		ImGui::SetNextWindowPos(ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f), ImGuiCond_Always, ImVec2(0.5f, 0.5f));
+		ImGui::SetNextWindowPos(
+			ImVec2(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.5f),
+			ImGuiCond_Always,
+			ImVec2(0.5f, 0.5f));
 	}
 	enabled = ImGui::BeginPopupModal(name.c_str(), &enabled,
 		ImGuiWindowFlags_NoMouseInputs | ImGuiWindowFlags_NoNavInputs | ImGuiWindowFlags_NoMove
