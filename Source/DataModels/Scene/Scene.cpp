2910caa3208c0d515f1e90df8ee2e2979b0721c7
gerard-queralt <gqueraltferre@gmail.com>
2023-05-25 20:40:45 +0200

    fix: children of copied object added to lists

--- Source/DataModels/Scene/Scene.cpp
+++ Source/DataModels/Scene/Scene.cpp
@@ -144,15 +144,6 @@ GameObject* Scene::DuplicateGameObject(const std::string& name, GameObject* newO
 
 	InsertGameObjectAndChildrenIntoSceneGameObjects(gameObject);
 
-	if (newObject->IsStatic())
-	{
-		loadedScene->AddStaticObject(gameObject);
-	}
-	else
-	{
-		loadedScene->AddNonStaticObject(gameObject);
-	}
-
 	return gameObject;
 }
 
@@ -730,6 +721,14 @@ void Scene::SetRoot(GameObject* newRoot)
 void Scene::InsertGameObjectAndChildrenIntoSceneGameObjects(GameObject* gameObject)
 {
 	sceneGameObjects.push_back(gameObject);
+	if (gameObject->IsStatic())
+	{
+		App->GetModule<ModuleScene>()->GetLoadedScene()->AddStaticObject(gameObject);
+	}
+	else
+	{
+		App->GetModule<ModuleScene>()->GetLoadedScene()->AddNonStaticObject(gameObject);
+	}
 	for (GameObject* children : gameObject->GetChildren())
 	{
 		InsertGameObjectAndChildrenIntoSceneGameObjects(children);
