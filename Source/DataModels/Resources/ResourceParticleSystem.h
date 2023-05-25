e84654bd0fef5d91d5819b7925739e03049ee75c
Sara Izquierdo <93783740+AuxPort66@users.noreply.github.com>
2023-05-25 16:14:30 +0200

    fix: Proper ResourceParticleSystem inheritance

--- Source/DataModels/Resources/ResourceParticleSystem.h
+++ Source/DataModels/Resources/ResourceParticleSystem.h
@@ -13,14 +13,31 @@ public:
 		const std::string& libraryPath);
 	~ResourceParticleSystem();
 
+	ResourceType GetType() const override;
+
+	void SaveImporterOptions(Json& meta) override {};
+	void LoadImporterOptions(Json& meta) override {};
+
+	void SaveLoadOptions(Json& meta) override {};
+	void LoadLoadOptions(Json& meta) override {};
+
 	unsigned int GetNumEmitters() const;
 	ParticleEmitter* GetEmitter(unsigned int emitterIndex) const;
 	void AddEmitter(std::unique_ptr<ParticleEmitter> emitter);
 	void ClearAllEmitters();
 
+protected:
+	void InternalLoad() override {};
+	void InternalUnload() override {};
+
 private:
     std::vector<std::unique_ptr<ParticleEmitter>> emitters;
 };
+
+inline ResourceType ResourceParticleSystem::GetType() const
+{
+	return ResourceType::ParticleSystem;
+}
 
 inline unsigned int ResourceParticleSystem::GetNumEmitters() const
 {
