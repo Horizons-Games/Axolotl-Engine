c0249b300d6d3661a1d23cf629d34193e2c338ff
Sara Izquierdo <93783740+AuxPort66@users.noreply.github.com>
2023-05-25 16:13:48 +0200

    feat[ComponentParticleSystem]: Add Serialization

--- Source/DataModels/Components/ComponentParticleSystem.cpp
+++ Source/DataModels/Components/ComponentParticleSystem.cpp
@@ -1,6 +1,11 @@
 #include "ComponentParticleSystem.h"
-
+#include "FileSystem/UniqueID.h"
+#include "Resources/ResourceParticleSystem.h"
 #include "ParticleSystem/EmitterInstance.h"
+#include "FileSystem/Json.h"
+#include "Application.h"
+#include "FileSystem/ModuleFileSystem.h"
+#include "FileSystem/ModuleResources.h"
 
 ComponentParticleSystem::ComponentParticleSystem(const bool active, GameObject* owner) :
 	Component(ComponentType::PARTICLE, active, owner, true), resource(nullptr)
@@ -19,10 +24,47 @@ ComponentParticleSystem::~ComponentParticleSystem()
 
 void ComponentParticleSystem::SaveOptions(Json& meta)
 {
+	// Do not delete these
+	meta["type"] = GetNameByType(type).c_str();
+	meta["active"] = (bool)active;
+	meta["removed"] = (bool)canBeRemoved;
+
+	UID uidParticleSystem = 0;
+	std::string assetPath = "";
+
+	if (resource)
+	{
+		uidParticleSystem = resource->GetUID();
+		assetPath = resource->GetAssetsPath();
+	}
+
+	meta["particleSystemUID"] = (UID)uidParticleSystem;
+	meta["assetPathParticleSystem"] = assetPath.c_str();
 }
 
 void ComponentParticleSystem::LoadOptions(Json& meta)
 {
+	// Do not delete these
+	type = GetTypeByName(meta["type"]);
+	active = (bool)meta["active"];
+	canBeRemoved = (bool)meta["removed"];
+	std::shared_ptr<ResourceParticleSystem> resourceParticleSystem;
+#ifdef ENGINE
+	std::string path = meta["assetPathParticleSystem"];
+	bool resourceExists = path != "" && App->GetModule<ModuleFileSystem>()->Exists(path.c_str());
+	if (resourceExists)
+	{
+		resourceParticleSystem = App->GetModule<ModuleResources>()->RequestResource<ResourceParticleSystem>(path);
+	}
+#else
+	UID uidParticleSystem = meta["particleSystemUID"];
+	resourceParticleSystem = App->GetModule<ModuleResources>()->SearchResource<ResourceParticleSystem>(uidParticleSystem);
+
+#endif
+	if (resourceParticleSystem)
+	{
+		SetResource(resourceParticleSystem);
+	}
 }
 
 void ComponentParticleSystem::Update()
