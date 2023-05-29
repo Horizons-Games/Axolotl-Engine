0fdbb6ae8b2f21e5f8cd0c72a1d595e14e257458
Alejandro Matus <moscowzero@gmail.com>
2023-05-29 21:03:17 +0200

    chore [UX/UI]: improve UI script 02

--- Game/Scripts/UIImageControl.cpp
+++ Game/Scripts/UIImageControl.cpp
@@ -3,11 +3,15 @@
 REGISTERCLASS(UIImageControl);
 
 UIImageControl::UIImageControl() : Script(), DisableImgObject(nullptr), EnableImgObject(nullptr), ImageComponent(nullptr),
-check(false)
+check(false), EnableImgObject02(nullptr), EnableImgObject03(nullptr), EnableImgObject04(nullptr)
 {
 	REGISTER_FIELD(EnableImgObject, GameObject*);
 	REGISTER_FIELD(DisableImgObject, GameObject*);
+	//hardcode to delete
 	REGISTER_FIELD(check, bool);
+	REGISTER_FIELD(EnableImgObject02, GameObject*);
+	REGISTER_FIELD(EnableImgObject03, GameObject*);
+	REGISTER_FIELD(EnableImgObject04, GameObject*);
 }
 
 void UIImageControl::Start()
@@ -21,35 +25,56 @@ void UIImageControl::Update(float deltaTime)
 
 	if (EnableImgObject != nullptr)
 	{
-		if (powerupStatus != true && check == true)
+		if (powerupStatus == false && check == true)
 		{
 			if (input->GetKey(SDL_SCANCODE_E) == KeyState::DOWN)
 			{
-				powerupTimer = time + 0.005;
 				powerupStatus = true;
 				EnableImgObject->Enable();
 			}
 		}
-		else if (powerupTimer <= deltaTime)
+		if (powerupStatus == true && powerupTimer <= 15.0f && check == true)
 		{
-			powerupTimer = 0;
+			powerupTimer += deltaTime;
+			if (powerupTimer >= 3.75f)
+			{
+				EnableImgObject->Disable();
+				EnableImgObject02->Enable();
+			}
+			if (powerupTimer >= 3.75f*2) 
+			{
+				EnableImgObject02->Disable();
+				EnableImgObject03->Enable();
+			}
+			if (powerupTimer >= 3.75f*3) 
+			{
+				EnableImgObject03->Disable();
+				EnableImgObject04->Enable();
+			}
+		}
+		else if (powerupStatus == true && powerupTimer >= 15.0f && check == true)
+		{
+			EnableImgObject04->Disable();
+			ENGINE_LOG("Entra con un tiempo de %f", powerupTimer);
+			powerupTimer = 0.0f;
 			powerupStatus = false;
-			EnableImgObject->Disable();
 		}
 
+		//This is to show the complete HUD just hardcode
 		if (input->GetKey(SDL_SCANCODE_H) == KeyState::DOWN)
 		{
 			if (hudStatus == false)
 			{
 				hudStatus = true;
 				EnableImgObject->Enable();
+				ENGINE_LOG("Complete HUD ON");
 			}
 			else
 			{
 				hudStatus = false;
 				EnableImgObject->Disable();
+				ENGINE_LOG("Complete HUD OFF");
 			}
-			
 		}
 	}
 }
--- Game/Scripts/UIImageControl.h
+++ Game/Scripts/UIImageControl.h
@@ -21,11 +21,14 @@ private:
 	float powerupTimer = 0;
 	float time = 0;
 	bool powerupStatus = false;
-	bool hudStatus = false;
-	bool check;
+	bool hudStatus = false; //This is to show the complete HUD just hardcode
+	bool check; //This is to show the complete HUD just hardcode
 
 	GameObject* DisableImgObject;
 	GameObject* EnableImgObject;
+	GameObject* EnableImgObject02; //Hardcode for pwr up
+	GameObject* EnableImgObject03; //Hardcode for pwr up
+	GameObject* EnableImgObject04; //Hardcode for pwr up
 	ComponentImage* ImageComponent;
 	ModuleInput* input;
 };
