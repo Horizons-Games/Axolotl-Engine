dd9ce34107306020497d544fdf7c29b40d37210b
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-05-31 02:37:12 +0200

    feat: CantinaNPCAnimationManager script

---
+++ Game/Scripts/CantinaNPCAnimationManager.cpp
@@ -0,0 +1,35 @@
+#include "CantinaNPCAnimationManager.h"
+#include "Components/ComponentAnimation.h"
+
+REGISTERCLASS(CantinaNPCAnimationManager);
+
+CantinaNPCAnimationManager::CantinaNPCAnimationManager() : Script(), animation(nullptr), activation(false), loopAnimation(false), percentageOfActivation(0)
+{
+	REGISTER_FIELD(loopAnimation, bool);
+	REGISTER_FIELD(percentageOfActivation, float);
+}
+
+void CantinaNPCAnimationManager::Start()
+{
+	animation = static_cast<ComponentAnimation*>(owner->GetComponent(ComponentType::ANIMATION));
+}
+
+void CantinaNPCAnimationManager::Update(float deltaTime)
+{
+	if(!loopAnimation || !activation)
+	{
+		if (activation)
+		{
+			animation->SetParameter("Activate", false);
+			activation = false;
+		}
+
+		int randomValue = std::rand() % 100 + 1;
+
+		if(randomValue <= percentageOfActivation)
+		{
+			animation->SetParameter("Activate", true);
+			activation = true;
+		}
+	}
+}
