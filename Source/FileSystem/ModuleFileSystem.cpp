3d7c87c7de4c69a8f0b0869b191a28a5c5b2e0b4
Gerard <gqueraltferre@gmail.com>
2023-06-07 02:38:42 +0200

    fix: game code

--- Source/FileSystem/ModuleFileSystem.cpp
+++ Source/FileSystem/ModuleFileSystem.cpp
@@ -27,7 +27,7 @@ bool ModuleFileSystem::Init()
 		return false;
 	}
 	PHYSFS_mount("Assets.zip", nullptr, 0);
-	PHYSFS_unmount(".", nullptr, 0);
+	PHYSFS_unmount(".");
 #endif // GAME
 	return true;
 }
