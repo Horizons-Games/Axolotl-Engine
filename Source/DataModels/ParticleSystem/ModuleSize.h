1587ad92b51b74645985d71d3d50b6a790267662
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-06-23 06:33:25 +0200

    feat: Get/Set ModuleSize

--- Source/DataModels/ParticleSystem/ModuleSize.h
+++ Source/DataModels/ParticleSystem/ModuleSize.h
@@ -17,8 +17,33 @@ public:
 
 	void DrawImGui() override;
 
+	bool IsRandom() const;
+	float2 GetSize() const;
+
+	void SetRandom(bool random);
+	void SetSize(float2 sizeOverTime);
+
 private:
 	bool random;
 	float2 sizeOverTime;
 };
 
+inline bool ModuleSize::IsRandom() const
+{
+	return random;
+}
+
+inline float2 ModuleSize::GetSize() const
+{
+	return sizeOverTime;
+}
+
+inline void ModuleSize::SetRandom(bool random)
+{
+	this->random = random;
+}
+
+inline void ModuleSize::SetSize(float2 sizeOverTime)
+{
+	this->sizeOverTime = sizeOverTime;
+}
