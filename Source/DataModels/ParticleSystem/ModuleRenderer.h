028bf1a50b5cb6ca6cd76b46a24dcfea7b56073f
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-06-23 06:33:47 +0200

    feat: Get/Set ModuleRenderer

--- Source/DataModels/ParticleSystem/ModuleRenderer.h
+++ Source/DataModels/ParticleSystem/ModuleRenderer.h
@@ -28,9 +28,19 @@ public:
 	void SetBlending(const BlendingMode& blending);
 	void SetTexture(const std::shared_ptr<ResourceTexture>& texture);
 
+	void SetTiles(int tilex, int tiley);
+	void SetSheetSpeed(float sheetSpeed);
+	void SetRandomFrame(bool randomFrame);
+	void SetFrameBlending(bool frameBlending);
+
 	Alignment GetAlignment() const;
 	BlendingMode GetBlending() const;
 	std::shared_ptr<ResourceTexture> GetTexture();
+
+	std::pair<int,int> GetTiles() const;
+	float GetSheetSpeed() const;
+	bool GetRandomFrame() const;
+	bool GetFrameBlending() const;
 
 private:
 	Alignment alignment;
@@ -67,6 +77,27 @@ inline void ModuleRenderer::SetTexture(const std::shared_ptr<ResourceTexture>& t
 	emitter->SetTexture(texture);
 }
 
+inline void ModuleRenderer::SetTiles(int tilex, int tiley)
+{
+	tiles[0] = tilex;
+	tiles[1] = tiley;
+}
+
+inline void ModuleRenderer::SetSheetSpeed(float sheetSpeed)
+{
+	this->sheetSpeed = sheetSpeed;
+}
+
+inline void ModuleRenderer::SetRandomFrame(bool randomFrame)
+{
+	this->randomFrame = randomFrame;
+}
+
+inline void ModuleRenderer::SetFrameBlending(bool frameBlending)
+{
+	this->frameBlending = frameBlending;
+}
+
 inline ModuleRenderer::Alignment ModuleRenderer::GetAlignment() const
 {
 	return alignment;
@@ -80,4 +111,24 @@ inline ModuleRenderer::BlendingMode ModuleRenderer::GetBlending() const
 inline std::shared_ptr<ResourceTexture> ModuleRenderer::GetTexture()
 {
 	return emitter->GetParticleTexture();
+}
+
+inline std::pair<int, int> ModuleRenderer::GetTiles() const
+{
+	return std::pair<int,int>(tiles[0], tiles[1]);
+}
+
+inline float ModuleRenderer::GetSheetSpeed() const
+{
+	return sheetSpeed;
+}
+
+inline bool ModuleRenderer::GetRandomFrame() const
+{
+	return randomFrame;
+}
+
+inline bool ModuleRenderer::GetFrameBlending() const
+{
+	return frameBlending;
 }
