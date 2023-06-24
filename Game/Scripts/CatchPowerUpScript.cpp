9aa657cb92ae8f52b2e595722c0183ac8e68ffe9
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2023-06-24 21:22:43 +0200

    feat: create catchUp

---
+++ Game/Scripts/CatchPowerUpScript.cpp
@@ -0,0 +1,17 @@
+#include "CatchPowerUpScript.h"
+
+#include "Components/ComponentScript.h"
+
+REGISTERCLASS(CatchPowerUpScript);
+
+CatchPowerUpScript::CatchPowerUpScript() : Script()
+{
+}
+
+void CatchPowerUpScript::Start()
+{
+}
+
+void CatchPowerUpScript::Update(float deltaTime)
+{
+}
