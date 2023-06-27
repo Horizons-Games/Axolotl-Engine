28254b407400c3146a3ad58038da17d98d492097
rodrigo samayoa <toti1998sama@gmail.com>
2023-06-27 17:28:09 +0200

    feat [WIP]: Add functionality to move agent

--- Source/DataModels/Components/ComponentAgent.cpp
+++ Source/DataModels/Components/ComponentAgent.cpp
@@ -1,5 +1,13 @@
 #include "ComponentAgent.h"
+
+#include "Application.h"
+#include "ModuleNavigation.h"
+
+#include "GameObject/GameObject.h"
+#include "Resources/ResourceNavMesh.h"
 #include "FileSystem/Json.h"
+
+#include "DetourCrowd/DetourCrowd.h"
 
 ComponentAgent::ComponentAgent(bool active, GameObject* owner) :
 	Component(ComponentType::AGENT, active, owner, true)
@@ -8,10 +16,25 @@ ComponentAgent::ComponentAgent(bool active, GameObject* owner) :
 
 ComponentAgent::~ComponentAgent()
 {
+	RemoveAgentFromCrowd();
 }
 
 void ComponentAgent::Update()
 {
+}
+
+void ComponentAgent::RemoveAgentFromCrowd()
+{
+	shouldAddAgentToCrowd = false;
+
+	/*if (App->GetModule<ModuleScene>()->GetLoadedScene() != GetOwner().scene)
+		return;*/
+	std::shared_ptr<ResourceNavMesh> navMesh = App->GetModule<ModuleNavigation>()->GetNavMesh();
+	if (!navMesh->IsGenerated() || agentId == -1)
+		return;
+
+	navMesh->GetCrowd()->removeAgent(agentId);
+	agentId = -1;
 }
 
 void ComponentAgent::InternalSave(Json& meta)
--- Source/DataModels/Components/ComponentAgent.h
+++ Source/DataModels/Components/ComponentAgent.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Auxiliar/Generics/Updatable.h"
 #include "Component.h"
+#include "Math/float3.h"
 
 class ComponentAgent: public Component, public Updatable
 {
@@ -10,8 +11,44 @@ public:
 
 	void Update() override;
 
+	void RemoveAgentFromCrowd();
+
+	float GetMaxSpeed() const;
+	float GetMaxAcceleration() const;
+	float3 GetTargetPosition() const;
+	bool IsAvoidingObstacle() const;
+
 private:
 	void InternalSave(Json& meta) override;
 	void InternalLoad(const Json& meta) override;
 
+	unsigned int targetPolygon = 0;		  // Target Polygon of the NavMesh to navigate
+	float3 targetPosition = float3::zero; // Target position of the NavMesh to navigate
+	int agentId = -1;					  // Agent identifier in NavMesh's crowd
+
+	float maxSpeed = 5.0f;
+	float maxAcceleration = 8.0f;
+	bool avoidingObstacle = true;
+	bool shouldAddAgentToCrowd = true;
+
 };
+
+inline float ComponentAgent::GetMaxSpeed() const
+{
+	return maxSpeed;
+}
+
+inline float ComponentAgent::GetMaxAcceleration() const
+{
+	return maxAcceleration;
+}
+
+inline float3 ComponentAgent::GetTargetPosition() const
+{
+	return targetPosition;
+}
+
+inline bool ComponentAgent::IsAvoidingObstacle() const
+{
+	return avoidingObstacle;
+}
