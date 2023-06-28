773d731c5b44b029c7611e9d0686f40223dc16a6
Oriol Sors <oriolsors@gmail.com>
2023-06-28 02:37:26 +0200

    refactor: controller function and player manager variable

--- Game/Scripts/PlayerManagerScript.cpp
+++ Game/Scripts/PlayerManagerScript.cpp
@@ -13,6 +13,7 @@ PlayerManagerScript::PlayerManagerScript() : Script(), playerAttack(20.0f), play
 	REGISTER_FIELD(playerAttack, float);
 	REGISTER_FIELD(playerDefense, float);
 	REGISTER_FIELD(playerSpeed, float);
+	REGISTER_FIELD(playerRotationSpeed, float);
 }
 
 float PlayerManagerScript::GetPlayerAttack() const
@@ -28,6 +29,11 @@ float PlayerManagerScript::GetPlayerDefense() const
 float PlayerManagerScript::GetPlayerSpeed() const
 {
 	return playerSpeed;
+}
+
+float PlayerManagerScript::GetPlayerRotationSpeed() const
+{
+	return playerRotationSpeed;
 }
 
 void PlayerManagerScript::IncreasePlayerAttack(float attackIncrease)
