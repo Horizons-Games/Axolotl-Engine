426c9195930d14357be026775b6d731615b80fb5
AlexisDieu <alexisdieu3@gmail.com>
2023-07-02 14:21:13 +0200

    fix : forget modification

--- Source/Shaders/component_line_vertex.glsl
+++ Source/Shaders/component_line_vertex.glsl
@@ -1,13 +1,24 @@
 #version 460
-layout(location = 0) in vec3 vertexPosition;
 
 layout(location = 0) uniform mat4 proj;
 layout(location = 1) uniform mat4 view;
 layout(location = 2) uniform mat4 model;
 
-out vec2 uv0;
+layout(location = 0) in vec3 vertexPosition;
+layout (location = 1) in vec2 texCoord;
+layout(location = 2) in vec3 vertexColor;
+
+uniform vec2 offset;
+uniform vec2 tiling;
+
+out vec2 TexCoord;
+out vec3 incolor;
+
 void main()
 {
- gl_Position = view * proj * model * vec4(vertexPosition, 1.0);
- //uv0 = vertex_position.zw;
+    TexCoord = texCoord*tiling+offset;
+
+    incolor = vertexColor;
+
+     gl_Position = view * proj * model * vec4(vertexPosition, 1.0);
 }
