c1a5490a20e48b873868f710915057ca60d009d3
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-07-13 00:43:43 +0200

    fix: change Debug only GetPosition to GetLocalPosition

--- Game/Scripts/RangedFastAttackBullet.cpp
+++ Game/Scripts/RangedFastAttackBullet.cpp
@@ -36,7 +36,7 @@ void RangedFastAttackBullet::Start()
 void RangedFastAttackBullet::Update(float deltaTime)
 {
 #ifdef DEBUG
-	Ray rayDebug(transform->GetPosition(), transform->GetLocalForward());
+	Ray rayDebug(transform->GetLocalPosition(), transform->GetLocalForward());
 	dd::arrow(rayDebug.pos, rayDebug.pos + rayDebug.dir * rayAttackSize, dd::colors::Red, 0.05f);
 #endif // DEBUG
 
