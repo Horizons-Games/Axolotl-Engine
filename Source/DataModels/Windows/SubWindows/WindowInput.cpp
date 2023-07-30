20ee9b868fc2ad29598c61b83e911c7c29b41118
Marc-Gonzalez-Moratona <80526953+MarcGonzalezMoratona@users.noreply.github.com>
2023-07-30 23:48:51 +0200

    refactor: ImGui selectable

--- Source/DataModels/Windows/SubWindows/WindowInput.cpp
+++ Source/DataModels/Windows/SubWindows/WindowInput.cpp
@@ -4,6 +4,17 @@
 #include "Application.h"
 #include "ModuleInput.h"
 
+namespace 
+{
+	const int numRows = 3;
+	const int buttonsPerRow = 4;
+	const int numButtons = numRows * buttonsPerRow;
+	const char* buttonLabels[numButtons] = { "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V" };
+	const int buttonScancodes[numButtons] = { SDL_SCANCODE_Q, SDL_SCANCODE_W, SDL_SCANCODE_E, SDL_SCANCODE_R,
+										  SDL_SCANCODE_A, SDL_SCANCODE_S, SDL_SCANCODE_D, SDL_SCANCODE_F,
+										  SDL_SCANCODE_Z, SDL_SCANCODE_X, SDL_SCANCODE_C, SDL_SCANCODE_V };
+}
+
 WindowInput::WindowInput() : SubWindow("Input")
 {
 }
@@ -16,25 +27,19 @@ void WindowInput::DrawWindowContents()
 {
 	ModuleInput* input = App->GetModule<ModuleInput>();
 
-	const int numRows = 3;
-	const int buttonsPerRow = 4;
-	const int numButtons = numRows * buttonsPerRow;
-	const char* buttonLabels[numButtons] = { "Q", "W", "E", "R", "A", "S", "D", "F", "Z", "X", "C", "V" };
-	const int buttonScancodes[numButtons] = { SDL_SCANCODE_Q, SDL_SCANCODE_W, SDL_SCANCODE_E, SDL_SCANCODE_R,
-											  SDL_SCANCODE_A, SDL_SCANCODE_S, SDL_SCANCODE_D, SDL_SCANCODE_F,
-											  SDL_SCANCODE_Z, SDL_SCANCODE_X, SDL_SCANCODE_C, SDL_SCANCODE_V };
 
     ImGuiTableFlags tableFlags = ImGuiTableFlags_SizingStretchSame | ImGuiTableFlags_Resizable | ImGuiTableFlags_NoBordersInBody;
+	ImGuiSelectableFlags selectableFlags = ImGuiSelectableFlags_Disabled;
 
 	if (ImGui::BeginTable("ButtonTable", buttonsPerRow, tableFlags))
 	{
 		for (int i = 0; i < numButtons; ++i)
 		{
 			ImGui::TableNextColumn();
-			ImGui::Selectable(buttonLabels[i], input->GetKey(buttonScancodes[i]) != KeyState::IDLE);
+			ImGui::Selectable(buttonLabels[i], input->GetKey(buttonScancodes[i]) != KeyState::IDLE, selectableFlags);
 		}
 		ImGui::EndTable();
 	}
 
-	ImGui::Selectable("SPACE", input->GetKey(SDL_SCANCODE_SPACE) != KeyState::IDLE);
+	ImGui::Selectable("SPACE", input->GetKey(SDL_SCANCODE_SPACE) != KeyState::IDLE, selectableFlags);
 }
