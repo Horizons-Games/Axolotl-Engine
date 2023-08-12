96087e65420ea99b1f2c6b42d296eba70bf08e30
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-08-12 21:02:43 +0200

    fix: JumpAttack animation sometimes not being set to false correctly

--- Game/Scripts/BixAttackScript.cpp
+++ Game/Scripts/BixAttackScript.cpp
@@ -266,14 +266,11 @@ void BixAttackScript::ResetAttackAnimations()
 
 		case AttackType::JUMPNORMAL:
 		case AttackType::JUMPFINISHER:
+			animation->SetParameter("IsJumpAttacking", false);
 			if (animation->GetActualStateName() == "BixJumpAttackRecovery" && !animation->IsPlaying())
 			{
 				isAttacking = false;
 				isJumpAttacking = false;
-			}
-			else if (animation->GetActualStateName() == "BixJumpAttack")
-			{
-				animation->SetParameter("IsJumpAttacking", false);
 			}
 			// There are some times in which the animations happen so quick and the first if is not entered,
 			// so I added this as a safe mesure because, if not, the player would be prevented of attacking,
