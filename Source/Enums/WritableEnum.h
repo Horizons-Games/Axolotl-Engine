703f6424b04a63ad5fa93e0e9dc385a50c29e829
Gerard <gqueraltferre@gmail.com>
2023-08-21 22:40:39 +0200

    perf: compute writable enum's names vector only once

--- Source/Enums/WritableEnum.h
+++ Source/Enums/WritableEnum.h
@@ -19,24 +19,31 @@ E FromString(const std::string&)
 }
 } // namespace
 
-#define WRITABLE_ENUM(enumName, ...)                                        \
-	enum class enumName                                                     \
-	{                                                                       \
-		__VA_ARGS__,                                                        \
-		UNKNOWN                                                             \
-	};                                                                      \
-	namespace                                                               \
-	{                                                                       \
-	template<>                                                              \
-	std::string ToString(enumName enumValue)                                \
-	{                                                                       \
-		return axo::detail::ToString(enumValue, #__VA_ARGS__);              \
-	}                                                                       \
-	template<>                                                              \
-	enumName FromString(const std::string& enumString)                      \
-	{                                                                       \
-		return axo::detail::FromString<enumName>(enumString, #__VA_ARGS__); \
-	}                                                                       \
+#define WRITABLE_ENUM(enumName, ...)                                                              \
+	enum class enumName                                                                           \
+	{                                                                                             \
+		__VA_ARGS__,                                                                              \
+		UNKNOWN                                                                                   \
+	};                                                                                            \
+                                                                                                  \
+	namespace axo::detail::enumName                                                               \
+	{                                                                                             \
+	std::vector<std::string> namesVector = FormatNamesVector(#__VA_ARGS__);                       \
+	}                                                                                             \
+                                                                                                  \
+	namespace                                                                                     \
+	{                                                                                             \
+	template<>                                                                                    \
+	std::string ToString(enumName enumValue)                                                      \
+	{                                                                                             \
+		return axo::detail::ToString(enumValue, axo::detail::enumName::namesVector);              \
+	}                                                                                             \
+                                                                                                  \
+	template<>                                                                                    \
+	enumName FromString(const std::string& enumString)                                            \
+	{                                                                                             \
+		return axo::detail::FromString<enumName>(enumString, axo::detail::enumName::namesVector); \
+	}                                                                                             \
 	}
 
 namespace axo::detail
@@ -44,18 +51,14 @@ namespace axo::detail
 std::vector<std::string> FormatNamesVector(std::string&& names);
 
 template<typename E>
-std::string ToString(E enumValue, std::string&& names)
+std::string ToString(E enumValue, const std::vector<std::string>& namesVector)
 {
-	std::vector<std::string> namesVector = FormatNamesVector(std::move(names));
-
 	return namesVector[static_cast<int>(enumValue)];
 }
 
 template<typename E>
-E FromString(const std::string& enumString, std::string&& names)
+E FromString(const std::string& enumString, const std::vector<std::string>& namesVector)
 {
-	std::vector<std::string> namesVector = FormatNamesVector(std::move(names));
-
 	std::string stringToLower = enumString;
 	std::transform(stringToLower.begin(), stringToLower.end(), stringToLower.begin(), ::tolower);
 
