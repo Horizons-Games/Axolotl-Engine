59169409eddbece47b17f410660a4bbd99f73f0b
Oriol Sors <oriolsors@gmail.com>
2023-08-22 18:32:26 +0200

    feat: init script structure

--- Game/Scripts/PlayerHackingUseScript.cpp
+++ Game/Scripts/PlayerHackingUseScript.cpp
@@ -1,2 +1,21 @@
 #include "StdAfx.h"
 #include "PlayerHackingUseScript.h"
+
+#include "Application.h"
+#include "ModuleInput.h"
+
+REGISTERCLASS(PlayerHackingUseScript);
+
+PlayerHackingUseScript::PlayerHackingUseScript()
+	: Script(), isHackingActive(false), hackingTag("Hackeable")
+{
+}
+
+void PlayerHackingUseScript::Start()
+{
+	input = App->GetModule<ModuleInput>();
+}
+
+void PlayerHackingUseScript::Update(float deltaTime)
+{
+}
--- Game/Scripts/PlayerHackingUseScript.h
+++ Game/Scripts/PlayerHackingUseScript.h
@@ -1,6 +1,31 @@
 #pragma once
 
 #include "Scripting\Script.h"
+
+class ModuleInput;
+
 class PlayerHackingUseScript : public Script
 {
+public:
+
+	PlayerHackingUseScript();
+	~PlayerHackingUseScript() override = default;
+
+	void Start() override;
+	void Update(float deltaTime) override;
+
+	bool IsHackingActive() const;
+
+private:
+
+	bool isHackingActive;
+
+	std::string hackingTag;
+
+	ModuleInput* input;
 };
+
+inline bool PlayerHackingUseScript::IsHackingActive() const
+{
+	return isHackingActive;
+}
