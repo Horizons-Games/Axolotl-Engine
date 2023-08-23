36c77ef64588e7d52f0e2bccf64628ad3065deac
Oriol Sors <oriolsors@gmail.com>
2023-08-23 22:09:52 +0200

    feat: generate combination in hackzone

--- Source/HackZoneScript.cpp
+++ Source/HackZoneScript.cpp
@@ -1,17 +1,53 @@
 #include "StdAfx.h"
 #include "HackZoneScript.h"
 
+#include <algorithm>
+#include <random>
+
 REGISTERCLASS(HackZoneScript);
 
-HackZoneScript::HackZoneScript() : Script()
+HackZoneScript::HackZoneScript() 
+	: Script()
 {
-
+	REGISTER_FIELD(influenceRadius, float);
+	REGISTER_FIELD(maxTime, float);
+	REGISTER_FIELD(sequenceSize, int);
 }
+
+
 
 void HackZoneScript::Start()
 {
+	keyCombination.reserve(sequenceSize);
+	buttonCombination.reserve(sequenceSize);
+
+	GenerateCombination();
 }
 
 void HackZoneScript::Update(float deltaTime)
 {
+}
+
+void HackZoneScript::GenerateCombination()
+{
+	std::vector<SDL_Scancode> allKeys = { SDL_SCANCODE_A, SDL_SCANCODE_B, SDL_SCANCODE_C };
+	std::vector<SDL_GameControllerButton> allButtons = { SDL_CONTROLLER_BUTTON_A, SDL_CONTROLLER_BUTTON_B };
+
+	std::random_device rd;
+	std::default_random_engine generator(rd());
+
+	keyCombination.clear();
+	buttonCombination.clear();
+
+	for (int i = 0; i < sequenceSize; ++i)
+	{
+		std::uniform_int_distribution<int> distribution(0, allKeys.size() - 1);
+		keyCombination.push_back(allKeys[distribution(generator)]);
+	}
+
+	for (int i = 0; i < sequenceSize; ++i)
+	{
+		std::uniform_int_distribution<int> distribution(0, allButtons.size() - 1);
+		buttonCombination.push_back(allButtons[distribution(generator)]);
+	}
 }
--- Source/HackZoneScript.h
+++ Source/HackZoneScript.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Scripting\Script.h"
+#include <vector>
+
 class HackZoneScript : public Script
 {
 public:
@@ -9,4 +11,43 @@ public:
 
 	void Start() override;
 	void Update(float deltaTime) override;
+
+	float GetInfluenceRadius() const;
+	float GetMaxTime() const;
+
+	const std::vector<SDL_Scancode>& GetKeyCombination() const;
+	const std::vector<SDL_GameControllerButton>& GetButtonCombination() const;
+
+private:
+
+	void GenerateCombination();
+
+private:
+
+	float influenceRadius;
+	float maxTime;
+	int sequenceSize;
+
+	std::vector<SDL_Scancode> keyCombination;
+	std::vector<SDL_GameControllerButton> buttonCombination;
 };
+
+inline float HackZoneScript::GetInfluenceRadius() const
+{
+	return influenceRadius;
+}
+
+inline float HackZoneScript::GetMaxTime() const
+{
+	return maxTime;
+}
+
+inline const std::vector<SDL_Scancode>& HackZoneScript::GetKeyCombination() const
+{
+	return keyCombination;
+}
+inline const std::vector<SDL_GameControllerButton>& HackZoneScript::GetButtonCombination() const
+{
+	return buttonCombination;
+}
+
