0bd15b38eb34a73dec7f06a80787bafcff036ed3
david castro salazar <david.cassa@hotmail.com>
2023-09-02 12:39:59 +0200

    Fix animation

--- Game/Scripts/PlayerAttackScript.cpp
+++ Game/Scripts/PlayerAttackScript.cpp
@@ -310,14 +310,12 @@ void PlayerAttackScript::ResetAttackAnimations()
 
 		case AttackType::JUMPNORMAL:
 		case AttackType::JUMPFINISHER:
+			animation->SetParameter("IsJumpAttacking", false);
 			if (animation->GetActualStateName() == "JumpAttackRecovery" && !animation->IsPlaying())
 			{
 				isAttacking = false;
 			}
-			else if (animation->GetActualStateName() == "JumpAttack")
-			{
-				animation->SetParameter("IsJumpAttacking", false);
-			}
+			
 			// There are some times in which the animations happen so quick and the first if is not entered,
 			// so I added this as a safe mesure because, if not, the player would be prevented of attacking,
 			// jumping and moving if the first if is not entered
