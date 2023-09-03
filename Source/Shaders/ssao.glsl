e88572df099a9809704c6f98652b096ff3ea6a0e
Miquel Prieto Moliné <mikipmoline@gmail.com>
2023-09-03 19:58:48 +0200

    feat[SSAO]: added camera to fragment shader

--- Source/Shaders/ssao.glsl
+++ Source/Shaders/ssao.glsl
@@ -5,7 +5,6 @@
 #define TANGENT_COLS 5
 
 in vec2 uv;
-in vec4 projection;
 out vec4 color;
 
 uniform sampler2D positions;
@@ -14,6 +13,18 @@ uniform sampler2D normals;
 uniform vec2 screenSize;
 uniform vec3 randomTangents[TANGENT_ROWS][TANGENT_COLS];
 uniform vec3 kernelSamples[KERNEL_SIZE];
+
+layout(std140, row_major, binding = 0) uniform Camera 
+{
+    mat4 proj;      // 16 // 00 (column 0)
+                    // 16 // 16 (column 1)
+                    // 16 // 32 (column 2)
+                    // 16 // 48 (column 3)
+    mat4 view;      // 16 // 64 (column 0)
+                    // 16 // 80 (column 1)
+                    // 16 // 96 (column 2)
+                    // 16 // 112 (column 3)
+};
 
 mat3 createTangentSpace(const vec3 normal, const vec3 randTangent)
 {
