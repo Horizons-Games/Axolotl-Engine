4807879f25874f6f0d73b81b23b209c5c72f2e9b
Sara <93783740+AuxPort66@users.noreply.github.com>
2023-09-03 20:48:31 +0200

    fix: Check InFront only with enemyselected

--- Game/Scripts/EntityDetection.cpp
+++ Game/Scripts/EntityDetection.cpp
@@ -120,9 +120,14 @@ void EntityDetection::SelectEnemy(float distanceFilter)
 			float minActualThresholdAngle = (angleActualSelected - angleThresholdEnemyIntersection);
 			float maxActualThresholdAngle = (angleActualSelected + angleThresholdEnemyIntersection);
 
-			bool inFrontOfActualSelected = 
-				angle <= maxActualThresholdAngle && originPosition.Distance(enemy->GetGlobalPosition()) < 
-				originPosition.Distance(enemySelected->GetGlobalPosition());
+
+			bool inFrontOfActualSelected = false;
+			if (enemySelected != nullptr) 
+			{
+				inFrontOfActualSelected =
+					angle <= maxActualThresholdAngle && originPosition.Distance(enemy->GetGlobalPosition()) <
+					originPosition.Distance(enemySelected->GetGlobalPosition());
+			}
 
 			if (enemySelected == nullptr || angle < minActualThresholdAngle || inFrontOfActualSelected)
 			{
