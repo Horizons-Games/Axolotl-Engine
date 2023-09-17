6fba5ee74fe6506fdf56c9b321c40595d90033da
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2023-09-17 23:07:56 +0200

    fix: resource not found

--- Source/DataModels/Batch/GeometryBatch.cpp
+++ Source/DataModels/Batch/GeometryBatch.cpp
@@ -936,6 +936,10 @@ void GeometryBatch::BindBatch(std::vector<GameObject*>& objects)
 		component->UpdatePalette();
 
 		ResourceInfo* resourceInfo = FindResourceInfo(component->GetMesh());
+		if (resourceInfo == nullptr)
+		{
+			continue;
+		}
 		std::shared_ptr<ResourceMesh> resource = resourceInfo->resourceMesh;
 
 		// find position in components vector
