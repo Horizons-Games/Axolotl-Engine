feb84dc4d4604f368e85daa2f93066e31c20c9ef
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2023-09-22 01:31:26 +0200

    refactor: unique bindCameraMethod

--- Source/DataModels/Components/ComponentLocalIBL.h
+++ Source/DataModels/Components/ComponentLocalIBL.h
@@ -56,7 +56,6 @@ private:
 	void Initialize();
 	void CreateCubemap();
 	void RenderToCubeMap(unsigned int cubemapTex, Program* usedProgram, int resolution, int mipmapLevel = 0);
-	void BindCameraToProgram(Program* program, Frustum& frustum, unsigned int uboCamera);
 	void CreateVAO();
 
 	GLuint frameBuffer;
