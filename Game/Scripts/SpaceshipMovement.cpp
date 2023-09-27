bc1d7d25d3123f24a98a6da10247a7661a2807ad
Oriol Murcia Catalan <oriolmurciacatalan@gmail.com>
2023-09-27 23:09:26 +0200

    feat: spaceship movement and rotation

---
+++ Game/Scripts/SpaceshipMovement.cpp
@@ -0,0 +1,118 @@
+#include "StdAfx.h"
+#include "SpaceshipMovement.h"
+
+#include "GameObject/GameObject.h"
+
+#include "Application.h"
+#include "ModuleInput.h"
+
+#include "Components/Component.h"
+#include "Components/ComponentRigidBody.h"
+#include "Components/ComponentTransform.h"
+
+REGISTERCLASS(SpaceshipMovement);
+
+SpaceshipMovement::SpaceshipMovement() : Script(), rigidbody(nullptr), forwardSpeed(20.0f), input(nullptr), XYSpeed(0.01f),
+meshTransform(nullptr), XrotationSpeed(0.1f), YrotationSpeed(0.1f), XrotationLimit(20.0f), YrotationLimit(2.0f)
+{
+	REGISTER_FIELD(forwardSpeed, float);
+	REGISTER_FIELD(XYSpeed, float);
+	REGISTER_FIELD(XrotationSpeed, float);
+	REGISTER_FIELD(YrotationSpeed, float);
+	REGISTER_FIELD(XrotationLimit, float);
+	REGISTER_FIELD(YrotationLimit, float);
+	REGISTER_FIELD(meshTransform, ComponentTransform*);
+}
+
+void SpaceshipMovement::Start()
+{
+	rigidbody = owner->GetComponent<ComponentRigidBody>()->GetRigidBody();
+	input = App->GetModule<ModuleInput>();
+}
+
+void SpaceshipMovement::Update(float deltaTime)
+{
+	horizontalDirection = input->GetLeftJoystickDirection().horizontalDirection;
+	verticalDirection = input->GetLeftJoystickDirection().verticalDirection;
+
+	Move(deltaTime);
+	Rotate(deltaTime);
+}
+
+void SpaceshipMovement::Move(float deltaTime)
+{
+	btVector3 movement(0, 0, forwardSpeed);
+	if (input->GetCurrentInputMethod() == InputMethod::GAMEPAD &&
+		(horizontalDirection != JoystickHorizontalDirection::NONE ||
+			verticalDirection != JoystickVerticalDirection::NONE))
+	{
+		btVector3 newMovement = btVector3(-input->GetLeftJoystickMovement().horizontalMovement,
+			-input->GetLeftJoystickMovement().verticalMovement, 0.0f) * deltaTime * XYSpeed;
+
+
+		movement = btVector3(newMovement.getX(), newMovement.getY(), movement.getZ());
+	}
+
+	rigidbody->setLinearVelocity(movement);
+}
+
+void SpaceshipMovement::Rotate(float deltaTime)
+{
+	Quat rotation = meshTransform->GetGlobalRotation();
+	float3 newRotation = float3::zero;
+	float3 currentRotation = math::RadToDeg(rotation.ToEulerXYZ());
+
+	if (input->GetCurrentInputMethod() == InputMethod::GAMEPAD &&
+		(horizontalDirection != JoystickHorizontalDirection::NONE ||
+			verticalDirection != JoystickVerticalDirection::NONE))
+	{
+		float xRotation = 0.0f;
+		float yRotation = 0.0f;
+
+		float xSign = math::Sign(input->GetLeftJoystickMovement().horizontalMovement);
+		float ySign = math::Sign(input->GetLeftJoystickMovement().verticalMovement);
+
+		if (currentRotation.z <= XrotationLimit && xSign == 1.0f)
+		{
+			newRotation = float3(newRotation.x, newRotation.y, xSign * deltaTime * XrotationSpeed);
+		}
+		if (currentRotation.z >= -XrotationLimit && xSign == -1.0f)
+		{
+			newRotation = float3(newRotation.x, newRotation.y, xSign * deltaTime * XrotationSpeed);
+		}
+		if (currentRotation.x <= YrotationLimit && ySign == 1.0f)
+		{
+			newRotation = float3(ySign * deltaTime * YrotationSpeed, newRotation.y, newRotation.z);
+		}
+		if (currentRotation.x >= -YrotationLimit && ySign == -1.0f)
+		{
+			newRotation = float3(ySign * deltaTime * YrotationSpeed, newRotation.y, newRotation.z);
+		}
+
+
+	}
+	else
+	{
+		if (currentRotation.z >= 1.0)
+		{
+			newRotation = float3(newRotation.x, newRotation.y, -1.0f * deltaTime * XrotationSpeed);
+		}
+		if (currentRotation.z <= -1.0f)
+		{
+			newRotation = float3(newRotation.x, newRotation.y, 1.0f * deltaTime * XrotationSpeed);
+		}
+		if (currentRotation.x >= 1.0f)
+		{
+			newRotation = float3(-1.0f * deltaTime * YrotationSpeed, newRotation.y, newRotation.z);
+		}
+		if (currentRotation.x <= -1.0f)
+		{
+			newRotation = float3(1.0f * deltaTime * YrotationSpeed, newRotation.y, newRotation.z);
+		}
+	}
+	newRotation = rotation.ToEulerXYZ() + newRotation;
+	meshTransform->SetGlobalRotation(math::RadToDeg(newRotation));
+
+	meshTransform->RecalculateLocalMatrix();
+	meshTransform->UpdateTransformMatrices();
+}
