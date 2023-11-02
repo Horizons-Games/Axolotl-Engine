8acc8bdf4f82d20157d0f8b4305897fb4e7c9d83
diegusmejia117 <diego.mejia@saei.mx>
2023-11-02 01:18:03 +0100

    chore[Art]: Shield VFX
    added shield VFX to boss, working on adding it to the miniboss in level 2

--- Game/Scripts/BossChargeAttackScript.cpp
+++ Game/Scripts/BossChargeAttackScript.cpp
@@ -287,7 +287,7 @@ void BossChargeAttackScript::SpawnRock(const float3& spawnPosition)
 
 	ComponentBreakable* newRockBreakable = newRock->GetComponent<ComponentBreakable>();
 
-	ComponentParticleSystem* newRockVFX = newRock->GetComponent<ComponentParticleSystem>();
+	
 
 	if (!newRock->GetChildren().empty())
 	{
