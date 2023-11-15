09228bd261de5214931b9b084896bcb167a184b8
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2023-11-15 14:47:18 +0100

    fix: planar reflection mip map level

--- Source/DataModels/Components/ComponentPlanarReflection.cpp
+++ Source/DataModels/Components/ComponentPlanarReflection.cpp
@@ -158,7 +158,7 @@ void ComponentPlanarReflection::InitBuffer()
 		glGenTextures(1, &reflectionTex);
 		glBindTexture(GL_TEXTURE_2D, reflectionTex);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, windowSize.first, windowSize.second, 0, GL_RGBA, GL_FLOAT, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
